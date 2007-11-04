021788ba016e888297cbd03e4caae48255912c18
simpleenigmainc <simpleenigmainc@29fdf216-9c39-0410-b332-13cbea8546cf>
2007-11-04 16:34:46 +0000

    Updated with simple eDoc code
    git-svn-id: http://erlyvideo.googlecode.com/svn/trunk@10 29fdf216-9c39-0410-b332-13cbea8546cf

--- src/gen_rtmp.erl
+++ src/gen_rtmp.erl
@@ -1,14 +1,11 @@
 %%%---------------------------------------------------------------------------------------
 %%% @author     Roberto Saccon
-%%% @author     Stuart Jackson <sjackson@simpleenigma.com> [http://erlsoft.org]
+%%% @author     Stuart Jackson <simpleenigmainc@gmail.com> [http://erlsoft.org]
 %%% @author     Luke Hubbard
-%%% @doc        IMAP server command processing
+%%% @doc        Generalized RTMP applicaiton behavior module
 %%% @reference  See <a href="http://erlyvideo.googlecode.com" target="_top">http://erlyvideo.googlecode.com</a> for more information
 %%% @end
 %%%---------------------------------------------------------------------------------------
-
-
-
 -module(gen_rtmp).
 -author('sjackson@simpleenigma.com').
 -include("../include/ems.hrl").
@@ -17,12 +14,23 @@
 
 -export([behaviour_info/1]).
 
+
+%%-------------------------------------------------------------------------
+%% @spec (Callbacks::atom()) -> CallBackList::list()
+%% @doc  List of require funcations in a RTMP application
+%% @hidden
+%% @end
+%%-------------------------------------------------------------------------
 behaviour_info(callbacks) -> [{createStream,3},{play,3},{stop,3},{pause,3},{deleteStream,3},{closeStream,3},{publish,3},{live,3},{append,3}];
 behaviour_info(_Other) -> undefined.
 
 
 
-
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a connect command and responds
+%% @end
+%%-------------------------------------------------------------------------
 connect(From, AMF, Channel) ->
     ?D("invoke - connect"),   
 	NewAMF = AMF#amf{
@@ -34,7 +42,11 @@ connect(From, AMF, Channel) ->
 			  ]},
 	gen_fsm:send_event(From, {send, {Channel,NewAMF}}).
 
-
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a createStream command and responds
+%% @end
+%%-------------------------------------------------------------------------
 createStream(From, AMF, Channel) -> 
     ?D("invoke - createStream"),   
 	NewAMF = AMF#amf{
@@ -43,9 +55,19 @@ createStream(From, AMF, Channel) ->
 	gen_fsm:send_event(From, {send, {Channel,NewAMF}}).
 
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a deleteStream command and responds
+%% @end
+%%-------------------------------------------------------------------------
 deleteStream(_From, _AMF, _Channel) ->  
     ?D("invoke - deleteStream").
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a play command and responds
+%% @end
+%%-------------------------------------------------------------------------
 play(From, AMF, Channel) -> 
     ?D("invoke - play"),
 	NextChannel = Channel#channel{id=4},
@@ -70,10 +92,20 @@ play(From, AMF, Channel) ->
 
 
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a pause command and responds
+%% @end
+%%-------------------------------------------------------------------------
 pause(From, _AMF, _Channel) -> 
     ?D("invoke - pause"),
     gen_fsm:send_event(From, {pause}). 
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a publish command and responds
+%% @end
+%%-------------------------------------------------------------------------
 publish(From, AMF, _Channel) -> 
     ?D("invoke - publish"),
 	Args = AMF#amf.args,
@@ -88,26 +120,51 @@ publish(From, AMF, _Channel) ->
 		_ -> ok
 	end. 
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a stop command and responds
+%% @end
+%%-------------------------------------------------------------------------
 stop(From, _AMF, _Channel) -> 
     ?D("invoke - stop"),
     gen_fsm:send_event(From, {stop}). 
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a live command and responds
+%% @end
+%%-------------------------------------------------------------------------
 live(From, _AMF, _Channel) -> 
     ?D("invoke - live"),
     gen_fsm:send_event(From, {stop}). 
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a append command and responds
+%% @end
+%%-------------------------------------------------------------------------
 append(From, _AMF, _Channel) -> 
     ?D("invoke - append"),
     gen_fsm:send_event(From, {stop}). 
 
 
 
+%%-------------------------------------------------------------------------
+%% @spec (From::pid(),AMF::tuple(),Channel::tuple) -> any()
+%% @doc  Processes a closeStream command and responds
+%% @end
+%%-------------------------------------------------------------------------
 closeStream(From, _AMF, _Channel) ->
     ?D("invoke - closeStream"),
     gen_fsm:send_event(From, {stop}). 
 
 
 
+%%-------------------------------------------------------------------------
+%% @spec (Name::string()) -> FileName::string()
+%% @doc ensures that the file name ends with a .flv extention
+%% @end
+%%-------------------------------------------------------------------------
 filename(Name) ->
 	case filename:extension(Name) of
 		".flv" -> Name;
@@ -116,5 +173,10 @@ filename(Name) ->
 	end.
 
 
+%%-------------------------------------------------------------------------
+%% @spec () -> any()
+%% @doc
+%% @end
+%%-------------------------------------------------------------------------
 generate_stream_id() ->  %% provisory !!!!!!!
    1.  %% TODO: replace by (distrubuted) gen_server to get a unique Id 
