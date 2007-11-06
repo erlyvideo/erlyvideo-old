217440f63ca5a0565fb28673eb45aeff3b06c275
king.selassie <king.selassie@29fdf216-9c39-0410-b332-13cbea8546cf>
2007-11-06 15:06:45 +0000

    Fixed a small bug in get_app_env that was causing code to always ignore value set and use default.
    git-svn-id: http://erlyvideo.googlecode.com/svn/trunk@44 29fdf216-9c39-0410-b332-13cbea8546cf

--- src/ems_app.erl
+++ src/ems_app.erl
@@ -73,7 +73,8 @@ init([Module]) ->
 %% Internal functions
 %%----------------------------------------------------------------------
 get_app_env(Opt, Default) ->
-    case application:get_env(application:get_application(), Opt) of
+	{ok, App} = application:get_application(),
+    case application:get_env(App, Opt) of
     {ok, Val} -> Val;
     _ ->
         case init:get_argument(Opt) of
@@ -81,3 +82,5 @@ get_app_env(Opt, Default) ->
         error       -> Default
         end
     end.
+
+
