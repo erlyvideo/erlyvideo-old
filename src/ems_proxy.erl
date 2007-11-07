02b6042231b13bcdb9f9e76e1c97b5dd566c5331
simpleenigmainc <simpleenigmainc@29fdf216-9c39-0410-b332-13cbea8546cf>
2007-11-07 22:47:06 +0000

    git-svn-id: http://erlyvideo.googlecode.com/svn/trunk@56 29fdf216-9c39-0410-b332-13cbea8546cf

--- src/ems_proxy.erl
+++ src/ems_proxy.erl
@@ -2,6 +2,9 @@
 %%% @author     Roberto Saccon <rsaccon@gmail.com> [http://rsaccon.com]
 %%% @author     Stuart Jackson <simpleenigmainc@gmail.com> [http://erlsoft.org]
 %%% @author     Luke Hubbard <luke@codegent.com> [http://www.codegent.com]
+%%% @doc        Debug Proxy server for RTMP and AMF packets
+%%% @reference  See <a href="http://erlyvideo.googlecode.com" target="_top">http://erlyvideo.googlecode.com</a> for more information
+%%% @end
 %%%
 %%%
 %%% The MIT License
@@ -27,19 +30,81 @@
 %%% THE SOFTWARE.
 %%%
 %%%
-%%% @doc        Generalized RTMP applicaiton behavior module
-%%% @reference  See <a href="http://erlyvideo.googlecode.com" target="_top">http://erlyvideo.googlecode.com</a> for more information
-%%% @end
 %%%---------------------------------------------------------------------------------------
 -module(ems_proxy).
 -author('rsaccon@gmail.comm').
 -author('simpleenigmainc@gmail.com').
 -author('luke@codegent.com').
+-include("../include/ems.hrl").
 
--export([]).
+-export([code_change/3,handle_call/3,handle_cast/2,handle_info/2,init/1,terminate/2]).
 
+-export([start_link/1,start_link/2,start_link/3]).
 
+-behavior(gen_server).
+%%-------------------------------------------------------------------------
+%% @spec (IP::tuple()) -> pid()
+%% @doc starts Debug Proxy server equivalant to start_link(IP,1935,"/tmp")
+%% @end
+%%-------------------------------------------------------------------------
+start_link(IP) -> start_link(IP,1935).
+%%-------------------------------------------------------------------------
+%% @spec (IP::tuple(),Port::integer()) -> pid()
+%% @doc starts Debug Proxy server equivalant to start_link(IP,Port,"/tmp")
+%% @end
+%%-------------------------------------------------------------------------
+start_link(IP,Port) -> start_link(IP,Port,"/tmp/").
+%%-------------------------------------------------------------------------
+%% @spec (IP::tuple(),Port::integer(),Dir::String) -> pid()
+%% @doc starts Debug Proxy server
+%% @end
+%%-------------------------------------------------------------------------
+start_link(IP,Port,Dir) -> 
+	?D({IP,Port,Dir}),
+	ok.
 
+%%-------------------------------------------------------------------------
+%% @spec (OldVsn::any(),State::any(),Extra::any()) -> any()
+%% @doc Upgrade or Downgrade code
+%% @end
+%%-------------------------------------------------------------------------
+code_change(_OldVsn,State,_Extra) -> {ok,State}.
 
+%%-------------------------------------------------------------------------
+%% @spec (Request::any(),From::pid(),State::any()) -> any()
+%% @doc Handles request from gen_server:call2,3 and gen_server:multi_call:2,3,4
+%% @end
+%%-------------------------------------------------------------------------
 
+handle_call(_Request,_From,State) -> {noreply,State}.
+%%-------------------------------------------------------------------------
+%% @spec (Request::any(),State::any()) -> any()
+%% @doc handles request from gen_server:cast/2
+%% @end
+%%-------------------------------------------------------------------------
 
+handle_cast(_Request,State) -> {noreply,State}.
+
+%%-------------------------------------------------------------------------
+%% @spec (Info::any(),State::any()) -> any()
+%% @doc Handles messages sent to server
+%% @end
+%%-------------------------------------------------------------------------
+handle_info({tcp,Socket, Bin},State) ->
+	?D({Socket,Bin}),
+	State;	
+handle_info(_Info,State) -> {noreply,State}.
+
+%%-------------------------------------------------------------------------
+%% @spec (Args::any()) -> any()
+%% @doc Initalization
+%% @end
+%%-------------------------------------------------------------------------
+init(_Args) -> {ok,[]}.
+
+%%-------------------------------------------------------------------------
+%% @spec (Reason::any(),State::any()) -> any()
+%% @doc stops Debug Proxy server
+%% @end
+%%-------------------------------------------------------------------------
+terminate(_Reason,_State) -> ok.
