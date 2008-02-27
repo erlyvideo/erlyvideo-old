386a31d54e81569f0a06adc5ff4d72359b0d9afe
simpleenigmainc <simpleenigmainc@29fdf216-9c39-0410-b332-13cbea8546cf>
2008-02-27 19:25:34 +0000

    Separate app file into app and sup files
    git-svn-id: http://erlyvideo.googlecode.com/svn/trunk@136 29fdf216-9c39-0410-b332-13cbea8546cf

--- src/ems_app.erl
+++ src/ems_app.erl
@@ -37,89 +37,21 @@
 -include("../include/ems.hrl").
 -behaviour(application).
 
-%% Internal API
--export([start_client/0]).
--export([get_app_env/2]).
+-define(APPLICATION,erlmedia).
 
-%% Application and Supervisor callbacks
--export([start/2, stop/1, init/1]).
+%% Application callbacks
+-export([start/2, stop/1]).
 
-%% A startup function for spawning new client connection handling FSM.
-%% To be called by the TCP listener process.
-start_client() ->
-    supervisor:start_child(ems_sup, []).
+%%--------------------------------------------------------------------
+%% @spec (Type::any(), Args::list()) -> any()
+%% @doc Starts ErlMedia Application
+%% @end 
+%%--------------------------------------------------------------------
 
-%%----------------------------------------------------------------------
-%% Application behaviour callbacks
-%%----------------------------------------------------------------------
-start(_Type, _Args) ->
-    ListenPort = get_app_env(listen_port, ?RTMP_PORT),
-    supervisor:start_link({local, ?MODULE}, ?MODULE, [ListenPort, ems_fsm]).
-
-stop(_S) ->
-    ok.
-
-%%----------------------------------------------------------------------
-%% Supervisor behaviour callbacks
-%%----------------------------------------------------------------------
-init([Port, Module]) ->
-    {ok,
-        {_SupFlags = {one_for_one, ?MAX_RESTART, ?MAX_TIME},
-            [ % EMS Cluster
-			  {   ems_cluster,
-		          {ems_cluster, start, []},
-		          permanent,
-		          1000,
-		          worker,
-		          [ems_cluster]
-		      },
-              % EMS Listener
-              {   ems_sup,                                 % Id       = internal id
-                  {ems_server,start_link,[Port,Module]},   % StartFun = {M, F, A}
-                  permanent,                               % Restart  = permanent | transient | temporary
-                  2000,                                    % Shutdown = brutal_kill | int() >= 0 | infinity
-                  worker,                                  % Type     = worker | supervisor
-                  [ems_server]                             % Modules  = [Module] | dynamic
-              },
-              % EMS instance supervisor
-              {   ems_client_sup,
-                  {supervisor,start_link,[{local, ems_sup}, ?MODULE, [Module]]},
-                  permanent,                               % Restart  = permanent | transient | temporary
-                  infinity,                                % Shutdown = brutal_kill | int() >= 0 | infinity
-                  supervisor,                              % Type     = worker | supervisor
-                  []                                       % Modules  = [Module] | dynamic
-              }
-            ]
-        }
-    };
-
-init([Module]) ->
-    {ok,
-        {_SupFlags = {simple_one_for_one, ?MAX_RESTART, ?MAX_TIME},
-            [
-              % TCP Client
-              {   undefined,                               % Id       = internal id
-                  {Module,start_link,[]},                  % StartFun = {M, F, A}
-                  temporary,                               % Restart  = permanent | transient | temporary
-                  2000,                                    % Shutdown = brutal_kill | int() >= 0 | infinity
-                  worker,                                  % Type     = worker | supervisor
-                  []                                       % Modules  = [Module] | dynamic
-              }
-            ]
-        }
-    }.
-
-
-%%----------------------------------------------------------------------
-%% Internal functions
-%%----------------------------------------------------------------------
-get_app_env(Opt, Default) ->	
-    case application:get_env(Opt) of
-    	{ok, Val} -> 
-			Val;
-    	_ ->
-        	case init:get_argument(Opt) of
-        		[[Val | _]] -> Val;
-        		error       -> Default
-        	end
-    end.
+start(_Type, _Args) -> ems_sup:start_link().
+%%--------------------------------------------------------------------
+%% @spec (Any::any()) -> any()
+%% @doc Stop ErlMedia Application
+%% @end 
+%%--------------------------------------------------------------------
+stop(_S) -> ok.
---
+++ src/ems_sup.erl
@@ -0,0 +1,144 @@
+%%% @author     Roberto Saccon <rsaccon@gmail.com> [http://rsaccon.com]
+%%% @author     Stuart Jackson <simpleenigmainc@gmail.com> [http://erlsoft.org]
+%%% @author     Luke Hubbard <luke@codegent.com> [http://www.codegent.com]
+%%% @copyright  2007 Luke Hubbard, Stuart Jackson, Roberto Saccon
+%%% @doc        Supervisor module
+%%% @reference  See <a href="http://erlyvideo.googlecode.com" target="_top">http://erlyvideo.googlecode.com</a> for more information
+%%% @end
+%%%
+%%%
+%%% The MIT License
+%%%
+%%% Copyright (c) 2007 Luke Hubbard, Stuart Jackson, Roberto Saccon
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in
+%%% all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%%% THE SOFTWARE.
+%%%
+%%%---------------------------------------------------------------------------------------
+-module(ems_sup).
+-author('rsaccon@gmail.com').
+-author('simpleenigmainc@gmail.com').
+-author('luke@codegent.com').
+-include("../include/ems.hrl").
+-behaviour(supervisor).
+
+-define(APPLICATION,erlmedia).
+
+-export ([init/1,start_link/0]).
+-export ([start_client/0]).
+-export ([get_app_env/1,get_app_env/2]).
+
+start_link() ->
+	ListenPort = get_app_env(listen_port, ?RTMP_PORT),
+	FSM = get_app_env(default_fsm, ems_fsm),
+	supervisor:start_link({local, ?MODULE}, ?MODULE, [ListenPort, FSM]).
+
+
+%%--------------------------------------------------------------------
+%% @spec (List::list()) -> any()
+%% @doc Initialize application
+%% @end 
+%%--------------------------------------------------------------------
+init([Port, Module]) ->
+    {ok,
+        {_SupFlags = {one_for_one, ?MAX_RESTART, ?MAX_TIME},
+            [ % EMS Cluster
+			  {   ems_cluster,
+		          {ems_cluster, start, []},
+		          permanent,
+		          1000,
+		          worker,
+		          [ems_cluster]
+		      },
+              % EMS Listener
+              {   ems_sup,                                 % Id       = internal id
+                  {ems_server,start_link,[Port,Module]},   % StartFun = {M, F, A}
+                  permanent,                               % Restart  = permanent | transient | temporary
+                  2000,                                    % Shutdown = brutal_kill | int() >= 0 | infinity
+                  worker,                                  % Type     = worker | supervisor
+                  [ems_server]                             % Modules  = [Module] | dynamic
+              },
+              % EMS instance supervisor
+              {   ems_client_sup,
+                  {supervisor,start_link,[{local, ems_client_sup}, ?MODULE, [Module]]},
+                  permanent,                               % Restart  = permanent | transient | temporary
+                  infinity,                                % Shutdown = brutal_kill | int() >= 0 | infinity
+                  supervisor,                              % Type     = worker | supervisor
+                  []                                       % Modules  = [Module] | dynamic
+              }
+            ]
+        }
+    };
+init([Module]) ->
+    {ok,
+        {_SupFlags = {simple_one_for_one, ?MAX_RESTART, ?MAX_TIME},
+            [
+              % TCP Client
+              {   undefined,                               % Id       = internal id
+                  {Module,start_link,[]},                  % StartFun = {M, F, A}
+                  temporary,                               % Restart  = permanent | transient | temporary
+                  2000,                                    % Shutdown = brutal_kill | int() >= 0 | infinity
+                  worker,                                  % Type     = worker | supervisor
+                  []                                       % Modules  = [Module] | dynamic
+              }
+            ]
+        }
+    }.
+
+
+%%----------------------------------------------------------------------
+%% Internal functions
+%%----------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% @spec () -> any()
+%% @doc A startup function for spawning new client connection handling FSM.
+%% To be called by the TCP listener process.
+%% @end 
+%%--------------------------------------------------------------------
+start_client() -> supervisor:start_child(ems_sup, []).
+
+
+
+%%--------------------------------------------------------------------
+%% @spec (Opt::atom()) -> any()
+%% @doc Gets application enviroment variable. User defined varaibles in 
+%% .config file override application default varabiles. Default [].
+%% @end 
+%%--------------------------------------------------------------------
+get_app_env(Opt) -> get_app_env(Opt, []).
+%%--------------------------------------------------------------------
+%% @spec (Opt::atom(), Default::any()) -> any()
+%% @doc Gets application enviroment variable. Returns Default if no 
+%% varaible named Opt is found. User defined varaibles in .config file
+%% override application default varabiles.
+%% @end 
+%%--------------------------------------------------------------------
+get_app_env(Opt, Default) ->
+	case lists:keysearch(?APPLICATION, 1, application:loaded_applications()) of
+		false -> application:load(?APPLICATION);
+		_ -> ok
+	end,
+	case application:get_env(?APPLICATION, Opt) of
+	{ok, Val} -> Val;
+	_ ->
+		case init:get_argument(Opt) of
+		[[Val | _]] -> Val;
+		error		-> Default
+		end
+	end.
