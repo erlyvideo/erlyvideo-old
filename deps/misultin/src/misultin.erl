bad9bf390490328349147a911c1f0e6ade4ebaec
Max Lapshin <max@maxidoors.ru>
2009-10-01 16:18:14 +0400

    bundled web server

---
+++ deps/misultin/src/misultin.erl
@@ -0,0 +1,237 @@
+% ==========================================================================================================
+% MISULTIN - Main
+%
+% >-|-|-(°>
+% 
+% Copyright (C) 2009, Roberto Ostinelli <roberto@ostinelli.net>, Sean Hinde.
+% All rights reserved.
+%
+% Code portions from Sean Hinde have been originally taken under BSD license from Trapexit at the address:
+% <http://www.trapexit.org/A_fast_web_server_demonstrating_some_undocumented_Erlang_features>
+%
+% BSD License
+% 
+% Redistribution and use in source and binary forms, with or without modification, are permitted provided
+% that the following conditions are met:
+%
+%  * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+%	 following disclaimer.
+%  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+%	 the following disclaimer in the documentation and/or other materials provided with the distribution.
+%  * Neither the name of the authors nor the names of its contributors may be used to endorse or promote
+%	 products derived from this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+% WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+% ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+% ==========================================================================================================
+-module(misultin).
+-behaviour(gen_server).
+-vsn('0.3').
+
+% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+% API
+-export([start_link/1, stop/0, create_acceptor/0]).
+
+% macros
+-define(SERVER, ?MODULE).
+
+% records
+-record(state, {
+	listen_socket,
+	port,
+	loop,
+	recv_timeout,
+	acceptor
+}).
+
+% includes
+-include("../include/misultin.hrl").
+
+
+% ============================ \/ API ======================================================================
+
+% Function: {ok,Pid} | ignore | {error, Error}
+% Description: Starts the server.
+start_link(Options) when is_list(Options) ->
+	gen_server:start_link({local, ?SERVER}, ?MODULE, [Options], []).
+
+% Function: -> ok
+% Description: Manually stops the server.
+stop() ->
+	gen_server:cast(?SERVER, stop).
+
+% Function: -> ok
+% Description: Send message to cause a new acceptor to be created
+create_acceptor() ->
+	gen_server:cast(?SERVER, create_acceptor).
+
+% ============================ /\ API ======================================================================
+
+
+% ============================ \/ GEN_SERVER CALLBACKS =====================================================
+
+% ----------------------------------------------------------------------------------------------------------
+% Function: -> {ok, State} | {ok, State, Timeout} | ignore | {stop, Reason}
+% Description: Initiates the server.
+% ----------------------------------------------------------------------------------------------------------
+init([Options]) ->
+	process_flag(trap_exit, true),
+	?DEBUG(info, "starting with Pid: ~p", [self()]),
+	% test and get options
+	OptionProps = [
+		{ip, {0, 0, 0, 0}, fun check_and_convert_string_to_ip/1, invalid_ip},
+		{port, 80, fun is_integer/1, port_not_integer},
+		{loop, {error, undefined_loop}, fun is_function/1, loop_not_function},
+		{backlog, 128, fun is_integer/1, backlog_not_integer},
+		{recv_timeout, 30*1000, fun is_integer/1, recv_timeout_not_integer}
+	],
+	OptionsVerified = lists:foldl(fun(OptionName, Acc) -> [get_option(OptionName, Options)|Acc] end, [], OptionProps),
+	case proplists:get_value(error, OptionsVerified) of
+		undefined ->
+			% get options
+			Ip = proplists:get_value(ip, OptionsVerified),
+			Port = proplists:get_value(port, OptionsVerified),
+			Loop = proplists:get_value(loop, OptionsVerified),
+			Backlog = proplists:get_value(backlog, OptionsVerified),
+			RecvTimeout = proplists:get_value(recv_timeout, OptionsVerified),
+			% ipv6 support
+			?DEBUG(debug, "ip address is: ~p", [Ip]),
+			InetOpt = case Ip of
+				{_, _, _, _} ->
+					% IPv4
+					inet;
+				{_, _, _, _, _, _, _, _} ->
+					% IPv6
+					inet6
+			end,
+			% ok, no error found in options -> create listening socket.
+			case gen_tcp:listen(Port, [binary, {packet, http}, InetOpt, {ip, Ip}, {reuseaddr, true}, {active, false}, {backlog, Backlog}]) of
+				{ok, ListenSocket} ->
+					% start listening
+					?DEBUG(debug, "starting listener loop", []),
+					% create acceptor
+					AcceptorPid = misultin_socket:start_link(ListenSocket, Port, Loop, RecvTimeout),
+					{ok, #state{listen_socket = ListenSocket, port = Port, loop = Loop, acceptor = AcceptorPid, recv_timeout = RecvTimeout}};
+				{error, Reason} ->
+					?DEBUG(error, "error starting: ~p", [Reason]),
+					% error
+					{stop, Reason}
+			end;
+		Reason ->
+			% error found in options
+			{stop, Reason}
+	end.
+
+% ----------------------------------------------------------------------------------------------------------
+% Function: handle_call(Request, From, State) -> {reply, Reply, State} | {reply, Reply, State, Timeout} |
+%									   {noreply, State} | {noreply, State, Timeout} |
+%									   {stop, Reason, Reply, State} | {stop, Reason, State}
+% Description: Handling call messages.
+% ----------------------------------------------------------------------------------------------------------
+
+% handle_call generic fallback
+handle_call(_Request, _From, State) ->
+	{reply, undefined, State}.
+
+% ----------------------------------------------------------------------------------------------------------
+% Function: handle_cast(Msg, State) -> {noreply, State} | {noreply, State, Timeout} | {stop, Reason, State}
+% Description: Handling cast messages.
+% ----------------------------------------------------------------------------------------------------------
+
+% manual shutdown
+handle_cast(stop, State) ->
+	?DEBUG(info, "manual shutdown..", []),
+	{stop, normal, State};
+
+% create
+handle_cast(create_acceptor, #state{listen_socket = ListenSocket, port = Port, loop = Loop, recv_timeout = RecvTimeout} = State) ->
+	?DEBUG(debug, "creating new acceptor process", []),
+	AcceptorPid = misultin_socket:start_link(ListenSocket, Port, Loop, RecvTimeout),
+	{noreply, State#state{acceptor = AcceptorPid}};
+
+% handle_cast generic fallback (ignore)
+handle_cast(_Msg, State) ->
+	?DEBUG(warning, "received unknown cast message: ~p", [_Msg]),
+	{noreply, State}.
+
+% ----------------------------------------------------------------------------------------------------------
+% Function: handle_info(Info, State) -> {noreply, State} | {noreply, State, Timeout} | {stop, Reason, State}
+% Description: Handling all non call/cast messages.
+% ----------------------------------------------------------------------------------------------------------
+
+% The current acceptor has died, respawn
+handle_info({'EXIT', Pid, _Reason}, #state{listen_socket = ListenSocket, port = Port, loop = Loop, acceptor = Pid, recv_timeout = RecvTimeout} = State) ->
+	?DEBUG(warning, "acceptor has died with reason: ~p, respawning", [_Reason]),
+	AcceptorPid = misultin_socket:start_link(ListenSocket, Port, Loop, RecvTimeout),
+	{noreply, State#state{acceptor = AcceptorPid}};
+
+% handle_info generic fallback (ignore)
+handle_info(_Info, State) ->
+	?DEBUG(warning, "received unknown info message: ~p", [_Info]),
+	{noreply, State}.
+
+% ----------------------------------------------------------------------------------------------------------
+% Function: terminate(Reason, State) -> void()
+% Description: This function is called by a gen_server when it is about to terminate. When it returns,
+% the gen_server terminates with Reason. The return value is ignored.
+% ----------------------------------------------------------------------------------------------------------
+terminate(_Reason, #state{listen_socket = ListenSocket, acceptor = AcceptorPid}) ->
+	?DEBUG(info, "shutting down server with Pid ~p", [self()]),
+	% kill acceptor - TODO: find a more gentle way to do so
+	exit(AcceptorPid, kill),
+	% stop gen_tcp
+	gen_tcp:close(ListenSocket),
+	terminated.
+
+% ----------------------------------------------------------------------------------------------------------
+% Function: code_change(OldVsn, State, Extra) -> {ok, NewState}
+% Description: Convert process state when code is changed.
+% ----------------------------------------------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+% ============================ /\ GEN_SERVER CALLBACKS =====================================================
+
+
+% ============================ \/ INTERNAL FUNCTIONS =======================================================
+
+% Function: -> false | IpTuple
+% Description: Checks and converts a string Ip to inet repr.
+check_and_convert_string_to_ip(Ip) ->
+	case inet_parse:address(Ip) of
+		{error, _Reason} ->
+			false;
+		{ok, IpTuple} ->
+			IpTuple
+	end.
+
+% Description: Validate and get misultin options.
+get_option({OptionName, DefaultValue, CheckAndConvertFun, FailTypeError}, Options) ->
+	case proplists:get_value(OptionName, Options) of
+		undefined ->
+			case DefaultValue of
+				{error, Reason} ->
+					{error, Reason};
+				Value -> 
+					{OptionName, Value}
+			end;
+		Value ->
+			case CheckAndConvertFun(Value) of
+				false ->
+					{error, {FailTypeError, Value}};
+				true -> 
+					{OptionName, Value};
+				OutValue ->
+					{OptionName, OutValue}
+			end
+	end.
+
+% ============================ /\ INTERNAL FUNCTIONS =======================================================
---
+++ deps/misultin/src/misultin_socket.erl
@@ -0,0 +1,377 @@
+% ==========================================================================================================
+% MISULTIN - Socket
+%
+% >-|-|-(°>
+% 
+% Copyright (C) 2009, Roberto Ostinelli <roberto@ostinelli.net>, Sean Hinde.
+% All rights reserved.
+%
+% Code portions from Sean Hinde have been originally taken under BSD license from Trapexit at the address:
+% <http://www.trapexit.org/A_fast_web_server_demonstrating_some_undocumented_Erlang_features>
+%
+% BSD License
+% 
+% Redistribution and use in source and binary forms, with or without modification, are permitted provided
+% that the following conditions are met:
+%
+%  * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+%	 following disclaimer.
+%  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+%	 the following disclaimer in the documentation and/or other materials provided with the distribution.
+%  * Neither the name of the authors nor the names of its contributors may be used to endorse or promote
+%	 products derived from this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+% WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+% ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+% ==========================================================================================================
+-module(misultin_socket).
+-vsn('0.3').
+
+% API
+-export([start_link/4]).
+
+% callbacks
+-export([listener/4]).
+
+% internale
+-export([socket_loop/1]).
+
+% macros
+-define(MAX_HEADERS_COUNT, 100).
+
+% records
+-record(c, {
+	sock,
+	port,
+	loop,
+	recv_timeout
+}).
+
+% includes
+-include("../include/misultin.hrl").
+
+
+% ============================ \/ API ======================================================================
+
+% Function: {ok,Pid} | ignore | {error, Error}
+% Description: Starts the socket.
+start_link(ListenSocket, ListenPort, Loop, RecvTimeout) ->
+	proc_lib:spawn_link(?MODULE, listener, [ListenSocket, ListenPort, Loop, RecvTimeout]).
+
+% Function: {ok,Pid} | ignore | {error, Error}
+% Description: Starts the socket.
+listener(ListenSocket, ListenPort, Loop, RecvTimeout) ->
+	case catch gen_tcp:accept(ListenSocket) of
+		{ok, Sock} ->
+			?DEBUG(debug, "accepted an incoming TCP connection, spawning controlling process", []),
+			Pid = spawn(fun () ->
+				receive
+					set ->
+						inet:setopts(Sock, [{active, true}]),
+						?DEBUG(debug, "activated controlling process", [])
+				after 60000 ->
+					exit({error, controlling_failed})
+				end,
+				% build connection record
+				{ok, {Addr, Port}} = inet:peername(Sock),
+				C = #c{sock = Sock, port = ListenPort, loop = Loop, recv_timeout = RecvTimeout},
+				% jump to state 'request'
+				?DEBUG(debug, "jump to state request", []),
+				request(C, #req{peer_addr = Addr, peer_port = Port})
+			end),
+			% set controlling process
+			gen_tcp:controlling_process(Sock, Pid),
+			Pid ! set,
+			% get back to accept loop
+			listener(ListenSocket, ListenPort, Loop, RecvTimeout);
+		_Else ->
+			?DEBUG(error, "accept failed error: ~p", [_Else]),
+			exit({error, accept_failed})
+	end.
+
+% ============================ /\ API ======================================================================
+
+
+% ============================ \/ INTERNAL FUNCTIONS =======================================================
+
+% REQUEST: wait for a HTTP Request line. Transition to state headers if one is received. 
+request(#c{sock = Sock, recv_timeout = RecvTimeout} = C, Req) ->
+	receive
+		{http, Sock, {http_request, Method, Path, Version}} ->
+			?DEBUG(debug, "received full headers of a new HTTP packet", []),
+			headers(C, Req#req{vsn = Version, method = Method, uri = Path, connection = default_connection(Version)}, []);
+		{http, Sock, {http_error, "\r\n"}} ->
+			request(C, Req);
+		{http, Sock, {http_error, "\n"}} ->
+			request(C, Req);
+		{http, Sock, _Other} ->
+			?DEBUG(debug, "tcp normal error treating request: ~p", [_Other]),
+			exit(normal)	
+	after RecvTimeout ->
+		?DEBUG(debug, "request timeout, sending error", []),
+		send(Sock, ?REQUEST_TIMEOUT_408)
+	end.
+
+% HEADERS: collect HTTP headers. After the end of header marker transition to body state.
+headers(C, Req, H) ->
+	headers(C, Req, H, 0).
+headers(#c{sock = Sock, recv_timeout = RecvTimeout} = C, Req, H, HeaderCount) when HeaderCount =< ?MAX_HEADERS_COUNT ->
+	receive
+		{http, Sock, {http_header, _, 'Content-Length', _, Val}} ->
+			headers(C, Req#req{content_length = Val}, [{'Content-Length', Val}|H], HeaderCount + 1);
+		{http, Sock, {http_header, _, 'Connection', _, Val}} ->
+			KeepAlive = keep_alive(Req#req.vsn, Val),
+			headers(C, Req#req{connection = KeepAlive}, [{'Connection', Val}|H], HeaderCount + 1);
+		{http, Sock, {http_header, _, Header, _, Val}} ->
+			headers(C, Req, [{Header, Val}|H], HeaderCount + 1);
+		{http, Sock, {http_error, "\r\n"}} ->
+			headers(C, Req, H, HeaderCount);
+		{http, Sock, {http_error, "\n"}} ->
+			headers(C, Req, H, HeaderCount);
+		{http, Sock, http_eoh} ->
+			body(C, Req#req{headers = lists:reverse(H)});
+		{http, Sock, _Other} ->
+			?DEBUG(debug, "tcp normal error treating headers: ~p", [_Other]),
+			exit(normal)
+	after RecvTimeout ->
+		?DEBUG(debug, "headers timeout, sending error", []),
+		send(Sock, ?REQUEST_TIMEOUT_408)
+	end;
+headers(C, Req, H, _HeaderCount) ->
+	body(C, Req#req{headers = lists:reverse(H)}).
+
+% default connection
+default_connection({1,1}) -> keep_alive;
+default_connection(_) -> close.
+
+% Shall we keep the connection alive? Default case for HTTP/1.1 is yes, default for HTTP/1.0 is no.
+keep_alive({1,1}, "close")		-> close;
+keep_alive({1,1}, "Close")		-> close;
+% string:to_upper is used only as last resort.
+keep_alive({1,1}, Head) ->
+	case string:to_upper(Head) of
+		"CLOSE" -> close;
+		_		-> keep_alive
+	end;
+keep_alive({1,0}, "Keep-Alive") -> keep_alive;
+keep_alive({1,0}, Head) ->
+	case string:to_upper(Head) of
+		"KEEP-ALIVE"	-> keep_alive;
+		_				-> close
+	end;
+keep_alive({0,9}, _)			-> close;
+keep_alive(_Vsn, _KA)			-> close.
+
+% BODY: collect the body of the HTTP request if there is one, and lookup and call the implementation callback.
+% Depending on whether the request is persistent transition back to state request to await the next request or exit.
+body(#c{sock = Sock, recv_timeout = RecvTimeout} = C, Req) ->
+	case Req#req.method of
+		'GET' ->
+			Close = handle_get(C, Req),
+			case Close of
+				close ->
+					gen_tcp:close(Sock);
+				keep_alive ->
+					% TODO: REMOVE inet:setopts(Sock, [{packet, http}]),
+					% inet:setopts(Sock, [{active, false}, {packet, 0}]),
+					request(C, #req{peer_addr = Req#req.peer_addr, peer_port = Req#req.peer_port})
+			end;
+		'POST' ->
+			case catch list_to_integer(Req#req.content_length) of 
+				{'EXIT', _} ->
+					% TODO: provide a fallback when content length is not or wrongly specified
+					?DEBUG(debug, "specified content length is not a valid integer number: ~p", [Req#req.content_length]),
+					send(Sock, ?CONTENT_LENGTH_REQUIRED_411),
+					exit(normal);
+				Len ->
+					inet:setopts(Sock, [{packet, raw}, {active, false}]),
+					case gen_tcp:recv(Sock, Len, RecvTimeout) of
+						{ok, Bin} ->
+							Close = handle_post(C, Req#req{body = Bin}),
+							case Close of
+								close ->
+									gen_tcp:close(Sock);
+								keep_alive ->
+									inet:setopts(Sock, [{packet, http}]),
+									request(C, #req{peer_addr = Req#req.peer_addr, peer_port = Req#req.peer_port})
+							end;
+						{error, timeout} ->
+							?DEBUG(debug, "request timeout, sending error", []),
+							send(Sock, ?REQUEST_TIMEOUT_408);	
+						_Other ->
+							?DEBUG(debug, "tcp normal error treating post: ~p", [_Other]),
+							exit(normal)
+					end
+			end;
+		_Other ->
+			?DEBUG(debug, "method not implemented: ~p", [_Other]),
+			send(Sock, ?NOT_IMPLEMENTED_501),
+			exit(normal)
+	end.
+
+% handle a get request
+handle_get(C, #req{connection = Conn} = Req) ->
+	case Req#req.uri of
+		{abs_path, Path} ->
+			{F, Args} = split_at_q_mark(Path, []),
+			call_mfa(C, Req#req{args = Args, uri = {abs_path, F}}),
+			Conn;
+		{absoluteURI, http, _Host, _, Path} ->
+			{F, Args} = split_at_q_mark(Path, []),
+			call_mfa(C, Req#req{args = Args, uri = {absoluteURI, F}}),
+			Conn;
+		{absoluteURI, _Other_method, _Host, _, _Path} ->
+			send(C#c.sock, ?NOT_IMPLEMENTED_501),
+			close;
+		{scheme, _Scheme, _RequestString} ->
+			send(C#c.sock, ?NOT_IMPLEMENTED_501),
+			close;
+		_  ->
+			send(C#c.sock, ?FORBIDDEN_403),
+			close
+	end.
+
+% handle a post request
+handle_post(C, #req{connection = Conn} = Req) ->
+	case Req#req.uri of
+		{abs_path, _Path} ->
+			call_mfa(C, Req),
+			Conn;
+		{absoluteURI, http, _Host, _, _Path} ->
+			call_mfa(C, Req),
+			Conn;
+		{absoluteURI, _Other_method, _Host, _, _Path} ->
+			send(C#c.sock, ?NOT_IMPLEMENTED_501),
+			close;
+		{scheme, _Scheme, _RequestString} ->
+			send(C#c.sock, ?NOT_IMPLEMENTED_501),
+			close;
+		_  ->
+			send(C#c.sock, ?FORBIDDEN_403),
+			close
+	end.
+
+% Description: Main dispatcher
+call_mfa(#c{sock = Sock, loop = Loop} = C, Request) ->
+	% spawn listening process for Request messages [only used to support stream requests]
+	SocketPid = spawn(?MODULE, socket_loop, [C]),
+	% create request
+	Req = misultin_req:new(Request, SocketPid),
+	% call loop
+	case catch Loop(Req) of
+		{'EXIT', _Reason} ->
+			?DEBUG(error, "worker crash: ~p", [_Reason]),
+			% kill listening socket
+			SocketPid ! shutdown,
+			% send response
+			send(Sock, ?INTERNAL_SERVER_ERROR_500),
+			% force exit
+			exit(normal);
+		{HttpCode, Headers0, Body} ->
+			% received normal response
+			?DEBUG(debug, "sending response", []),
+			% kill listening socket
+			SocketPid ! shutdown,
+			% flatten body [optimization since needed for content length]
+			BodyBinary = convert_to_binary(Body),
+			% provide response
+			Headers = add_content_length(Headers0, BodyBinary),
+			Enc_headers = enc_headers(Headers),
+			Resp = ["HTTP/1.1 ", integer_to_list(HttpCode), " OK\r\n", Enc_headers, <<"\r\n">>, BodyBinary],
+			send(Sock, Resp);
+		{raw, Body} ->
+			send(Sock, Body);
+		_ ->
+			% loop exited normally, kill listening socket
+			SocketPid ! shutdown
+	end.
+
+% Description: Ensure Body is binary.
+convert_to_binary(Body) when is_list(Body) ->
+	list_to_binary(lists:flatten(Body));
+convert_to_binary(Body) when is_binary(Body) ->
+	Body;
+convert_to_binary(Body) when is_atom(Body) ->
+	list_to_binary(atom_to_list(Body)).
+
+% Description: Socket loop for stream responses
+socket_loop(#c{sock = Sock} = C) ->
+	receive
+		{stream_head, HttpCode, Headers} ->
+			?DEBUG(debug, "sending stream head", []),
+			Enc_headers = enc_headers(Headers),
+			Resp = ["HTTP/1.1 ", integer_to_list(HttpCode), " OK\r\n", Enc_headers, <<"\r\n">>],
+			send(Sock, Resp),
+			socket_loop(C);
+		{stream_data, Body} ->
+			?DEBUG(debug, "sending stream data", []),
+			send(Sock, Body),
+			socket_loop(C);
+		stream_close ->
+			?DEBUG(debug, "closing stream", []),
+			close(Sock);
+		shutdown ->
+			?DEBUG(debug, "shutting down socket loop", []),
+			shutdown
+	end.
+
+% Description: Add content length
+add_content_length(Headers, Body) ->
+	case proplists:get_value('Content-Length', Headers) of
+		undefined ->
+			[{'Content-Length', size(Body)}|Headers];
+		false ->
+			Headers
+	end.
+
+% Description: Encode headers
+enc_headers([{Tag, Val}|T]) when is_atom(Tag) ->
+	[atom_to_list(Tag), ": ", enc_header_val(Val), "\r\n"|enc_headers(T)];
+enc_headers([{Tag, Val}|T]) when is_list(Tag) ->
+	[Tag, ": ", enc_header_val(Val), "\r\n"|enc_headers(T)];
+enc_headers([]) ->
+	[].
+enc_header_val(Val) when is_atom(Val) ->
+	atom_to_list(Val);
+enc_header_val(Val) when is_integer(Val) ->
+	integer_to_list(Val);
+enc_header_val(Val) ->
+	Val.
+
+% Split the path at the ?
+split_at_q_mark([$?|T], Acc) ->
+	{lists:reverse(Acc), T};
+split_at_q_mark([H|T], Acc) ->
+	split_at_q_mark(T, [H|Acc]);
+split_at_q_mark([], Acc) ->
+	{lists:reverse(Acc), []}.
+
+% TCP send
+send(Sock, Data) ->
+	?DEBUG(debug, "sending data: ~p", [Data]),
+	case gen_tcp:send(Sock, Data) of
+		ok ->
+			ok;
+		{error, _Reason} ->
+			?DEBUG(debug, "worker crash: ~p", [_Reason]),
+			exit(normal)
+	end.
+
+% TCP close
+close(Sock) ->
+	?DEBUG(debug, "closing socket", []),
+	case gen_tcp:close(Sock) of
+		ok ->
+			ok;
+		{error, _Reason} ->
+			?DEBUG(debug, "could not close socket: ~p", [_Reason]),
+			exit(normal)
+	end.
+	
+% ============================ /\ INTERNAL FUNCTIONS =======================================================
