3ce7f524107ede45cd157919bec5cde6c899502f
Max Lapshin <max@maxidoors.ru>
2009-10-09 01:56:25 +0400

    Now can read mkv integers

--- src/formats/mkv.erl
+++ src/formats/mkv.erl
@@ -33,12 +33,16 @@
 -module(mkv).
 -author('max@maxidoors.ru').
 -include("../../include/ems.hrl").
+-export([init/1, read_frame/1]).
+-behaviour(gen_format).
+
 
 init(#video_player{header = undefined} = Player) -> 
-  init(Player#video_player{frames = ets:new(frames, [ordered_set, {keypos, 2}])});
+  % init(Player#video_player{frames = ets:new(frames, [ordered_set, {keypos, 2}])});
 
 
-init(#video_player{header = Header, device = IoDev, frames = FrameTable} = Player) -> 
+% init(#video_player{frames = _FrameTable} = Player) -> 
+  read_element(Player#video_player{pos = 0}),
   {ok, Player}.
 
 
@@ -52,3 +56,32 @@ read_frame(#video_player{pos = undefined, frames = FrameTable} = Player) ->
 read_frame(#video_player{}) ->
   {ok, done}.
 
+
+match_int(#video_player{pos = Pos, device = IoDev}, Bytes) ->
+  {ok, Data} = file:pread(IoDev, Pos, Bytes),
+  BitSize = Bytes*7,
+  <<_:Bytes/bits, Int:BitSize/integer>> = iolist_to_binary(Data),
+  {ok, Int, Bytes}.
+  
+  
+decode_int(Player, <<1:1, Int:7>>) -> {ok, Int, 1};
+decode_int(Player, <<0:1, 1:1, _/bits>>) -> match_int(Player, 2);
+decode_int(Player, <<0:2, 1:1, _/bits>>) -> match_int(Player, 3);
+decode_int(Player, <<0:3, 1:1, _/bits>>) -> match_int(Player, 4);
+decode_int(Player, <<0:4, 1:1, _/bits>>) -> match_int(Player, 5);
+decode_int(Player, <<0:5, 1:1, _/bits>>) -> match_int(Player, 6);
+decode_int(Player, <<0:6, 1:1, _/bits>>) -> match_int(Player, 7);
+decode_int(Player, <<0:7, 1:1>>)      -> match_int(Player, 8).
+  
+
+next_int(#video_player{pos = Pos, device = IoDev} = Player) ->
+  case file:pread(IoDev, Pos, 1) of
+    {ok, Data} -> decode_int(Player, iolist_to_binary(Data));
+    {eof} -> eof
+  end.
+
+read_element(#video_player{pos = Pos, device = IoDev} = Player) ->
+  {ok, ClassId, IdBytes} = next_int(Player),
+  {ok, Size, SizeBytes} = next_int(Player#video_player{pos = Pos + IdBytes}),
+  ?D({"Read", ClassId, Size})
+  .
