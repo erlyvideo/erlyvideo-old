2781f7c7ff75b8d25c66b1ad31d8fb188e44ca45
Max Lapshin <max@maxidoors.ru>
2009-11-27 00:15:59 +0300

    Added module inititialization

--- src/ems.erl
+++ src/ems.erl
@@ -48,7 +48,9 @@
 %%--------------------------------------------------------------------
 start() -> 
 	io:format("Starting ErlMedia ...~n"),
-	application:start(?APPLICATION).
+	Start = application:start(?APPLICATION),
+	start_modules(),
+	Start.
 
 
 %%--------------------------------------------------------------------
@@ -58,6 +60,7 @@ start() ->
 %%--------------------------------------------------------------------
 stop() ->
 	io:format("Stopping ErlMedia ...~n"),
+	stop_modules(),
 	application:stop(?APPLICATION),
 	application:unload(?APPLICATION).
 
@@ -148,6 +151,34 @@ respond_to(Module, Command, Arity) ->
 		_ -> false
 	end.
   
+start_modules() ->
+  start_modules(ems:get_var(applications, [])).
+  
+start_modules([]) -> 
+  ok;
+start_modules([Module|Modules]) ->
+  case respond_to(Module, start, 0) of
+    true ->
+      ?D({"Init module", Module}),
+      ok = Module:start();
+    _ -> ok
+  end,
+  start_modules(Modules).
+
+
+stop_modules() ->
+  stop_modules(ems:get_var(applications, [])).
+
+stop_modules([]) -> 
+  ok;
+stop_modules([Module|Modules]) ->
+  case respond_to(Module, stop, 0) of
+    true ->
+      ?D({"Stop module", Module}),
+      ok = Module:stop();
+    _ -> ok
+  end,
+  stop_modules(Modules).
 
 %%--------------------------------------------------------------------
 %% @spec (Opt::atom(), Command::atom(), Arity::integer()) -> any()
