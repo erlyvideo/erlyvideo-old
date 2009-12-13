891092c203aca3a4648b7f0db090d43253a823e4
Max Lapshin <max@maxidoors.ru>
2009-12-14 00:10:24 +0300

    save alias mapping

--- src/ems_app.erl
+++ src/ems_app.erl
@@ -70,7 +70,7 @@ init_vhosts([{Name, Host} | Hosts]) ->
   true = ets:insert(vhosts, {Name, [{name, Name} | Host]}),
   lists:foreach(fun(Hostname) ->
     true = ets:insert(vhosts, {Hostname, Name})
-  end, proplists:get_value(hostname, Host)),
+  end, proplists:get_value(hostname, Host, [])),
   init_vhosts(Hosts).
 
 
