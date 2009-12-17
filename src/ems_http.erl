a2ac593d9f410b1b94cdcd984e9d19b352ed19e5
Max Lapshin <max@maxidoors.ru>
2009-12-17 17:20:26 +0300

    MPEG TS push acceptor was broken because of VHOSTS

--- src/ems_http.erl
+++ src/ems_http.erl
@@ -13,53 +13,53 @@ stop() ->
 % callback on request received
 handle_http(Req) ->	
   random:seed(now()),
-  handle(Req:get(method), Req:resource([urldecode]), Req).
+  handle(ems:host(Req:host()), Req:get(method), Req:resource([urldecode]), Req).
 
 
-handle('GET', [], Req) ->
+handle(Host, 'GET', [], Req) ->
   localhost,
   erlydtl:compile("wwwroot/index.html", index_template),
   
   Query = Req:parse_qs(),
   io:format("GET / ~p~n", [Query]),
   File = proplists:get_value("file", Query, "video.mp4"),
-  case file:list_dir(file_play:file_dir(Req:host())) of
+  case file:list_dir(file_play:file_dir(Host)) of
     {ok, FileList} -> ok;
     {error, Error} -> 
       FileList = [],
       error_logger:error_msg("Invalid HTTP root directory: ~p (~p)~n", [file_play:file_dir(Req:host()), Error])
   end,
-  Secret = ems:get_var(secret_key, Req:host(), undefined),
+  Secret = ems:get_var(secret_key, Host, undefined),
   {ok, Index} = index_template:render([
     {files, FileList},
-    {hostname, ems:get_var(host, Req:host(), "rtmp://localhost")},
+    {hostname, ems:get_var(host, Host, "rtmp://localhost")},
     {live_id, uuid:to_string(uuid:v4())},
     {url, File},
     {session, json_session:encode([{channels, [10, 12]}, {user_id, 5}], Secret)}]),
   Req:ok([{'Content-Type', "text/html; charset=utf8"}], Index);
 
 
-handle('GET', ["admin"], Req) ->
+handle(Host, 'GET', ["admin"], Req) ->
   erlydtl:compile("wwwroot/admin.html", admin_template),
   % {ok, Contents} = file:read_file("player/player.html"),
 
-  Entries = media_provider:entries(),
+  Entries = media_provider:entries(Host),
   {ok, Index} = admin_template:render([
   {entries, Entries}]),
   Req:ok([{'Content-Type', "text/html; charset=utf8"}], Index);
 
 
-handle('GET', ["chat.html"], Req) ->
+handle(Host, 'GET', ["chat.html"], Req) ->
   erlydtl:compile("wwwroot/chat.html", chat_template),
-  Secret = ems:get_var(secret_key, Req:host(), undefined),
+  Secret = ems:get_var(secret_key, Host, undefined),
   {ok, Index} = chat_template:render([
-    {hostname, ems:get_var(host, Req:host(), "rtmp://localhost")},
+    {hostname, ems:get_var(host, Host, "rtmp://localhost")},
     {session, json_session:encode([{channels, [10, 12]}, {user_id, 5}], Secret)}
   ]),
   Req:ok([{'Content-Type', "text/html; charset=utf8"}], Index);
 
   
-handle('POST', ["open", ChunkNumber], Req) ->
+handle(_Host, 'POST', ["open", ChunkNumber], Req) ->
   error_logger:info_msg("Request: open/~p.\n", [ChunkNumber]),
   SessionId = generate_session_id(),
   <<Timeout>> = Req:get(body),
@@ -67,7 +67,7 @@ handle('POST', ["open", ChunkNumber], Req) ->
   error_logger:info_msg("Opened session ~p, timeout ~p.\n", [SessionId, Timeout]),
   Req:ok([{'Content-Type', ?CONTENT_TYPE}, ?SERVER_HEADER], [SessionId, "\n"]);
   
-handle('POST', ["idle", SessionId, SequenceNumber], Req) ->
+handle(_Host, 'POST', ["idle", SessionId, SequenceNumber], Req) ->
   % error_logger:info_msg("Request: idle/~p/~p.\n", [SessionId, SequenceNumber]),
   case ets:match_object(rtmp_sessions, {SessionId, '$2'}) of
       [{SessionId, Rtmp}] ->
@@ -81,7 +81,7 @@ handle('POST', ["idle", SessionId, SequenceNumber], Req) ->
   end;
 
 
-handle('POST', ["send", SessionId, SequenceNumber], Req) ->
+handle(_Host, 'POST', ["send", SessionId, SequenceNumber], Req) ->
   % error_logger:info_msg("Request: send/~p/~p.\n", [SessionId, SequenceNumber]),
   case ets:match_object(rtmp_sessions, {SessionId, '$2'}) of
       [{SessionId, Rtmp}] ->
@@ -96,34 +96,34 @@ handle('POST', ["send", SessionId, SequenceNumber], Req) ->
   end;
   
   
-handle('POST', ["close", SessionId, ChunkNumber], Req) ->
+handle(_Host, 'POST', ["close", SessionId, ChunkNumber], Req) ->
     error_logger:info_msg("Request: close/~p/~p.\n", [SessionId, ChunkNumber]),
     Req:stream(<<0>>),
     Req:stream(close);
     
-handle('POST', ["fcs", "ident", ChunkNumber], Req) ->
+handle(_Host, 'POST', ["fcs", "ident", ChunkNumber], Req) ->
     error_logger:info_msg("Request: ident/~p.\n", [ChunkNumber]),
     Req:ok([{'Content-Type', ?CONTENT_TYPE}, ?SERVER_HEADER], "0.1");
     
-handle('POST', ["fcs", "ident2"], Req) ->
+handle(_Host, 'POST', ["fcs", "ident2"], Req) ->
     error_logger:info_msg("Request: ident2.\n"),
     Req:ok([{'Content-Type', ?CONTENT_TYPE}, ?SERVER_HEADER], "0.1");
   
-handle('POST', ["channels", ChannelS, "message"], Req) ->
+handle(_Host, 'POST', ["channels", ChannelS, "message"], Req) ->
   Message = proplists:get_value("message", Req:parse_post()),
   Channel = list_to_integer(ChannelS),
   rtmp_listener:send_to_channel(Channel, Message),
   Req:respond(200, [{"Content-Type", "text/plain"}], "200 OK\n");
 
-handle('POST', ["users", UserS, "message"], Req) ->
+handle(_Host, 'POST', ["users", UserS, "message"], Req) ->
   Message = proplists:get_value("message", Req:parse_post()),
   User = list_to_integer(UserS),
   rtmp_listener:send_to_user(User, Message),
   Req:respond(200, [{"Content-Type", "text/plain"}], "200 OK\n");
 
   
-handle('GET', ["stream", Name], Req) ->
-  case media_provider:play(Name) of
+handle(Host, 'GET', ["stream", Name], Req) ->
+  case media_provider:play(Host, Name) of
     {ok, PlayerPid} ->
       mpeg_ts:play(Name, PlayerPid, Req);
     {notfound} ->
@@ -132,7 +132,7 @@ handle('GET', ["stream", Name], Req) ->
       Req:respond(500, [{"Content-Type", "text/plain"}], "500 Internal Server Error.~n Failed to start video player: ~p~n ~p: ~p", [Reason, Name, Req])
   end;
   
-handle('GET', Path, Req) ->
+handle(_Host, 'GET', Path, Req) ->
   FileName = filename:absname(filename:join(["wwwroot" | Path])),
   case filelib:is_regular(FileName) of
     true ->
@@ -142,15 +142,15 @@ handle('GET', Path, Req) ->
       Req:respond(404, [{"Content-Type", "text/plain"}], "404 Page not found. ~p: ~p", [Path, Req])
   end;
 
-handle('PUT', ["stream", Name], Req) ->
+handle(Host, 'PUT', ["stream", Name], Req) ->
   ?D({"Stream", Name}),
-  Stream = media_provider:create(Name, mpeg_ts_passive),
+  Stream = media_provider:create(Host, Name, mpeg_ts_passive),
   gen_tcp:controlling_process(Req:socket(), Stream),
   gen_server:call(Stream, {set_socket, Req:socket()}),
   exit(leave);
   
 % handle the 404 page not found
-handle(_, Path, Req) ->
+handle(_Host, _, Path, Req) ->
 	Req:respond(404, [{"Content-Type", "text/plain"}], "404 Page not found. ~p: ~p", [Path, Req]).
 
 
