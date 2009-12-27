330a8eeb0344f20070b107f328954417362d28bd
Max Lapshin <max@maxidoors.ru>
2009-12-27 19:45:21 +0300

    erlang-amf to submodule

--- deps/erlang-amf/src/amf.hrl
+++
@@ -1,8 +0,0 @@
--ifndef(_AMF).
--define(_AMF, true).
-
--record(amf_packet, {version, headers, messages}).
--record(amf_header, {name, must_understand, body}).
--record(amf_message, {target, response, body}).
-
--endif.
