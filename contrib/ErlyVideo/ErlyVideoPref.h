7df55219bc99d6481ec27b8e052605f012194311
Alexander Zhouravlev <bofh@mail.ru>
2010-01-13 13:42:47 +0300

    Empty project for Mac OS X ErlyVideo system preferences pane.

---
+++ contrib/ErlyVideo/ErlyVideoPref.h
@@ -0,0 +1,19 @@
+//
+//  ErlyVideoPref.h
+//  ErlyVideo
+//
+//  Created by sK0T on 13.01.10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface ErlyVideoPref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ contrib/ErlyVideo/ErlyVideoPref.m
@@ -0,0 +1,18 @@
+//
+//  ErlyVideoPref.m
+//  ErlyVideo
+//
+//  Created by sK0T on 13.01.10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "ErlyVideoPref.h"
+
+
+@implementation ErlyVideoPref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
