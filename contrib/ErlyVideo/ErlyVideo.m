0c3fda14aec5731baa8431da18f161ba80bab9ff
Max Lapshin <max@maxidoors.ru>
2010-01-15 15:09:18 +0300

    Preliminary epmd connect version

--- contrib/ErlyVideo/ErlyVideo.m
+++ contrib/ErlyVideo/ErlyVideo.m
@@ -7,20 +7,46 @@
 //
 
 #import "ErlyVideo.h"
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 @implementation ErlyVideo
 
 static BOOL FakeStatus = NO;
 
++ (void)load
+{
+	erl_init();
+}
+
 + (BOOL)isActive
 {
+	struct sockaddr_in address;
+	int epmd;
+	
+	
+	epmd = socket(AF_INET, SOCK_STREAM, 0);
+	if (epmd < 0) return NO;
+	
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr ("127.0.0.1");
+	address.sin_port = htons(4369);
+	
+	if (connect(epmd, &address, sizeof(address)) < 0) {
+		return NO;
+	}
+	
+	//write(epmd, 
+	
 	return FakeStatus;
 }
 
 + (BOOL)installed
 {
-	return NO;
+	return [[NSFileManager defaultManager] fileExistsAtPath:@"/Library/ErlyVideo/ebin/erlmedia.app"];
 }
 
 + (int)port
@@ -42,6 +68,11 @@ static BOOL FakeStatus = NO;
 	[(NSObject *)sender performSelector:@selector(erlyVideoStatusChanged)
 							 withObject:nil
 							 afterDelay:1.0];
+	NSTask *task = [[NSTask alloc] init];
+	[task setLaunchPath:@"/usr/bin/make"];
+	[task setArguments:[NSArray arrayWithObjects:@"-f", @"/Library/ErlyVideo", @"start", nil];
+	[task launch];
+	
 }
 
 @end
