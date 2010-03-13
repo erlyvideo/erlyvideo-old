b23cb694cce12985d0d31d0a1f96181064ec0f46
Max Lapshin <max@maxidoors.ru>
2010-03-13 14:14:09 +0300

    Working with processes

--- src/http_file.erl
+++ src/http_file.erl
@@ -1,20 +1,44 @@
 -module(http_file).
 
--export([open/2, pread/3, close/1]).
+-export([open/2, init/2, pread/3, close/1, loop/1]).
 
 
 -record(http_file, {
   url,
   request_id,
-  options
+  options,
+  lala
 }).
 
 
 open(URL, Options) ->
-  #http_file{url = URL, options = Options}.
+  spawn_link(?MODULE, init, [URL, Options]).
+  
+
+init(URL, Options) ->
+  ?MODULE:loop(#http_file{url = URL, options = Options}).
+  
+
+loop(#http_file{} = File) ->
+  receive
+    {pread, Offset, Limit, Sender, Ref} ->
+      {ok, Result} = internal_pread(File, Offset, Limit),
+      Sender ! {ok, Result, Ref},
+      ?MODULE:loop(File);
+    Else ->
+      io:format("Unknown message: ~p~n", [Else]),
+      ok
+  end.
   
   
-pread(#http_file{url = URL}, Offset, Limit) ->
+pread(File, Offset, Limit) ->
+  Ref = erlang:make_ref(),
+  File ! {pread, Offset, Limit, self(), Ref},
+  receive
+    {ok, Result, Ref} -> {ok, Result}
+  end.
+  
+internal_pread(#http_file{url = URL}, Offset, Limit) ->
   Range = lists:flatten(io_lib:format("bytes=~p-~p", [Offset, Offset+Limit])),
   {ok, RequestID} = http:request(get, {URL, [{"Range", Range}]}, [], [{sync, false},{receiver, self()},{body_format,binary},{stream,self}]),
   wait_response(RequestID, <<>>, Limit).
@@ -24,6 +48,9 @@ pread(#http_file{url = URL}, Offset, Limit) ->
 wait_response(RequestID, Buffer, Limit) when size(Buffer) >= Limit ->
   {Result, _} = split_binary(Buffer, Limit),
   httpc:cancel_request(RequestID),
+  receive
+    {http, {RequestID, stream_end, _}} -> ok
+  end,  
   {ok, Result};
 
 wait_response(RequestID, Buffer, Limit) ->
@@ -32,7 +59,7 @@ wait_response(RequestID, Buffer, Limit) ->
       wait_response(RequestID, <<Buffer/binary, Bin/binary>>, Limit);
     {http, {RequestID, stream_end, _}} ->
       Buffer;
-    Message ->
+    {http, {RequestID, stream_start, _}} ->
       wait_response(RequestID, Buffer, Limit)
   end.
 
--- test.erl
+++ test.erl
@@ -7,9 +7,12 @@ main([]) ->
   % URL = "http://s3.amazonaws.com/erlyvideo/test/video.mp4",
   URL = "http://erlang.org/",
   Limit = 100,
-  File = http_file:open(URL, []),
+  File = http_file:open("http://erlang.org/", []),
   {ok, Result} = http_file:pread(File, 100, Limit),
-  io:format("~p ~s~n", [size(Result), Result]).
+  
+  File2 = http_file:open("http://ya.ru/", []),
+  {ok, Result2} = http_file:pread(File2, 100, Limit),
+  io:format("~s~n---------~n ~s~n", [Result, Result2]).
   
   
   
