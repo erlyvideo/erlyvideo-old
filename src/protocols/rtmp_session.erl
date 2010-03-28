78bbae1a8876d989dba250b00222c4d2cde2db93
Max Lapshin <max@maxidoors.ru>
2010-03-29 02:12:21 +0400

    rtmp_session debugs

--- src/protocols/rtmp_session.erl
+++ src/protocols/rtmp_session.erl
@@ -400,12 +400,10 @@ handle_info({ems_stream, StreamId, {notfound, _Reason}}, StateName, #rtmp_sessio
   
 
 handle_info({ems_stream, StreamId, play_complete, LastDTS}, StateName, #rtmp_session{socket = Socket} = State) ->
-  ?D({"Send play complete"}),
   rtmp_lib:play_complete(Socket, StreamId, [{duration, LastDTS}]),
   {next_state, StateName, State};
   
 handle_info({ems_stream, StreamId, seek_notify, Timestamp}, StateName, #rtmp_session{socket = Socket} = State) ->
-  ?D({"Send seek notify"}),
   rtmp_lib:seek_notify(Socket, StreamId, Timestamp),
   {next_state, StateName, State};
 
