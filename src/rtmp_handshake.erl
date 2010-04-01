ed06b594192e0fd3c0fde78b83780c99317ab6c9
Max Lapshin <max@maxidoors.ru>
2010-04-01 21:22:12 +0400

    Added detection of digest offset

--- src/rtmp_handshake.erl
+++ src/rtmp_handshake.erl
@@ -1,6 +1,12 @@
 %%% @private
 %%% @author     Max Lapshin <max@maxidoors.ru> [http://erlyvideo.org]
-%%% @author     All those guys, that have found how to sign handshake
+%%% @author     All those guys, that have found how to sign handshake from red5:
+%%% @author Jacinto Shy II (jacinto.m.shy@ieee.org)
+%%% @author Steven Zimmer (stevenlzimmer@gmail.com)
+%%% @author Gavriloaie Eugen-Andrei <crtmpserver@gmail.com>
+%%% @author Ari-Pekka Viitanen
+%%% @author Paul Gregoire
+%%% @author Tiago Jacobs 
 %%% @copyright  2009 Max Lapshin
 %%% @doc        RTMP handshake module
 %%% @reference  See <a href="http://erlyvideo.org/rtmp" target="_top">http://erlyvideo.org/rtmp</a> for more information
@@ -134,12 +140,35 @@
 16#08,16#72,16#52,16#a7,16#98,16#42,16#95,16#7b,16#b7,16#e7,16#10,16#fe,16#db,16#54,16#34,16#fb,
 16#91,16#24,16#1c,16#07,16#fb,16#9c,16#ce,16#d0,16#46,16#cf,16#c4,16#9d,16#09,16#49,16#24,16#ec>>).
 
--define(KEYSERVER, <<"Genuine Adobe Flash Media Server 001",
+-define(GENUINE_FMS_KEY, <<"Genuine Adobe Flash Media Server 001",
                         16#f0,16#ee,16#c2,16#4a,16#80,16#68,16#be,16#e8,16#2e,16#00,16#d0,16#d1,
 16#02,16#9e,16#7e,16#57,16#6e,16#ec,16#5d,16#2d,16#29,16#80,16#6f,16#ab,16#93,16#b8,16#e6,16#36,
 16#cf,16#eb,16#31,16#ae>>).
 
+-define(GENUINE_FP_KEY, <<"Genuine Adobe Flash Player 001",
+          16#F0, 16#EE, 16#C2, 16#4A, 16#80, 16#68, 16#BE, 16#E8,
+          16#2E, 16#00, 16#D0, 16#D1, 16#02, 16#9E, 16#7E, 16#57,
+          16#6E, 16#EC, 16#5D, 16#2D, 16#29, 16#80, 16#6F, 16#AB,
+          16#93, 16#B8, 16#E6, 16#36, 16#CF, 16#EB, 16#31, 16#AE>>).
 
+
+-define(DH_MODULUS_BYTES, <<16#ff, 16#ff, 16#ff, 16#ff, 16#ff,
+          		16#ff, 16#ff, 16#ff, 16#c9, 16#0f, 16#da, 16#a2, 16#21,
+          		16#68, 16#c2, 16#34, 16#c4, 16#c6, 16#62, 16#8b, 16#80,
+          		16#dc, 16#1c, 16#d1, 16#29, 16#02, 16#4e, 16#08, 16#8a,
+          		16#67, 16#cc, 16#74, 16#02, 16#0b, 16#be, 16#a6, 16#3b,
+          		16#13, 16#9b, 16#22, 16#51, 16#4a, 16#08, 16#79, 16#8e,
+          		16#34, 16#04, 16#dd, 16#ef, 16#95, 16#19, 16#b3, 16#cd,
+          		16#3a, 16#43, 16#1b, 16#30, 16#2b, 16#0a, 16#6d, 16#f2,
+          		16#5f, 16#14, 16#37, 16#4f, 16#e1, 16#35, 16#6d, 16#6d,
+          		16#51, 16#c2, 16#45, 16#e4, 16#85, 16#b5, 16#76, 16#62,
+          		16#5e, 16#7e, 16#c6, 16#f4, 16#4c, 16#42, 16#e9, 16#a6,
+          		16#37, 16#ed, 16#6b, 16#0b, 16#ff, 16#5c, 16#b6, 16#f4,
+          		16#06, 16#b7, 16#ed, 16#ee, 16#38, 16#6b, 16#fb, 16#5a,
+          		16#89, 16#9f, 16#a5, 16#ae, 16#9f, 16#24, 16#11, 16#7c,
+          		16#4b, 16#1f, 16#e6, 16#49, 16#28, 16#66, 16#51, 16#ec,
+          		16#e6, 16#53, 16#81, 16#ff, 16#ff, 16#ff, 16#ff, 16#ff,
+          		16#ff, 16#ff, 16#ff>>).
 
 s1() ->
 	?HANDSHAKE.
@@ -150,25 +179,48 @@ c1() ->
 c2(_S2) ->
   ?HANDSHAKE.
 
+
+client_version(<<T:32, V1, V2, V3, V4, _/binary>>) ->
+  {T, V1, V2, V3, V4}.
+
 % Flash from 10.0.32.18
-clientDigest(<<_:5/binary, 0, 3, 2, _:764/binary, P1/unsigned, P2/unsigned, P3/unsigned, P4/unsigned, _/binary>> = C1) ->
+clientDigest(<<_:772/binary, P1/unsigned, P2/unsigned, P3/unsigned, P4/unsigned, _/binary>> = C1, version2) ->
 	Offset = (P1+P2+P3+P4) rem 728 + 776,
-	<<_:Offset/binary, Seed:32/binary, _/binary>> = C1,
-  Seed;
+	<<First:Offset/binary, Seed:32/binary, Last/binary>> = C1,
+  {First, Seed, Last};
 
 
 % Flash before 10.0.32.18
-clientDigest(<<_:8/binary, P1/unsigned, P2/unsigned, P3/unsigned, P4/unsigned, _/binary>> = C1) ->
+clientDigest(<<_:8/binary, P1/unsigned, P2/unsigned, P3/unsigned, P4/unsigned, _/binary>> = C1, version1) ->
 	Offset = (P1+P2+P3+P4) rem 728 + 12,
-	<<_:Offset/binary, Seed:32/binary, _/binary>> = C1,
-  Seed.
+	<<First:Offset/binary, Seed:32/binary, Last/binary>> = C1,
+  {First, Seed, Last}.
+
+validateClientScheme(C1) ->
+  case validateClientScheme(C1, version1) of
+    true -> version1;
+    false -> case validateClientScheme(C1, version2) of
+      true -> version2;
+      false -> undefined
+    end
+  end.
+
+validateClientScheme(C1, Version) ->
+  {First, ClientDigestBin, Last} = clientDigest(C1, Version),
+  ClientDigest = binary_to_list(ClientDigestBin),
+  <<Key:30/binary, _/binary>> = ?GENUINE_FP_KEY,
+  GuessDigest = hmac256:digest(Key, <<First/binary, Last/binary>>),
+  ClientDigest == GuessDigest.
 
 % Special case for VLC
 s2(<<0:64, _:1528/binary>> = C1) ->
   C1;
 
 s2(C1) ->
-  ServerDigest = hmac256:digest(?KEYSERVER, clientDigest(C1)),
+  Version = validateClientScheme(C1),
+  io:format("Handshake version: ~p, ~p~n", [client_version(C1), Version]),
+  {_, ClientDigest, _} = clientDigest(C1, version2),
+  ServerDigest = hmac256:digest(?GENUINE_FMS_KEY, ClientDigest),
   <<S2:1504/binary, _/binary>> = s1(),
   ServerSign = hmac256:digest(ServerDigest, S2),
   [S2, ServerSign].
