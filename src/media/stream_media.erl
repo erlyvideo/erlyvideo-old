5d09d3a4ae2160dcb4b4fa1902f594ddf5ac79e7
Max Lapshin <max@maxidoors.ru>
2010-04-05 15:22:34 +0400

    Cannot fight with large timestamps

--- src/media/stream_media.erl
+++ src/media/stream_media.erl
@@ -328,7 +328,7 @@ handle_info(Message, State) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 handle_frame(#video_frame{dts = DTS} = Frame, #media_info{last_dts = undefined} = Recorder) ->
-  handle_frame(Frame, Recorder#media_info{last_dts = DTS});
+  handle_frame(Frame, Recorder#media_info{last_dts = 1000}); % Just not to appear negative dts
 
 handle_frame(#video_frame{dts = DTS} = Frame, #media_info{ts_delta = undefined, last_dts = LastDTS} = Recorder) ->
   ?D({"New instance of stream", LastDTS, DTS}),
