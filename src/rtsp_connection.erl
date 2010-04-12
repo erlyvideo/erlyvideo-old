62c613e5580341558c1e3f996676da3856e50dc6
Max Lapshin <max@secondspin.ru>
2010-04-12 19:21:30 +0400

    next fix for launchpad

--- src/rtsp_connection.erl
+++ src/rtsp_connection.erl
@@ -2,7 +2,7 @@
 -author('Max Lapshin <max@maxidoors.ru>').
 
 -define(D(X), io:format("DEBUG ~p:~p ~p~n",[?MODULE, ?LINE, X])).
--include_lib("ertsp/include/rtsp.hrl").
+-include("../ertsp/include/rtsp.hrl").
 -include_lib("erlmedia/include/video_frame.hrl").
 
 % -include("../include/rtsp.hrl").
