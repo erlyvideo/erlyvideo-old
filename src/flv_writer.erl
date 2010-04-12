f533563b0ec9f8412b2b6bbecdee542ab71f7fcb
Max Lapshin <max@maxidoors.ru>
2010-04-12 19:30:41 +0400

    FLV writer now can write to any streamer

--- src/flv_writer.erl
+++ src/flv_writer.erl
@@ -7,36 +7,43 @@
 -export([start_link/1, init/1, writer/1]).
 
 -record(flv_file_writer, {
-  file,
+  writer,
   base_dts
 }).
 
 
 start_link(FileName) ->
   {ok, spawn_link(?MODULE, init, [[FileName]])}.
+
+init([Writer]) when is_function(Writer) ->
+	Writer(flv:header()),
+	?MODULE:writer(#flv_file_writer{writer = Writer});
+  
   
 init([FileName]) ->
 	ok = filelib:ensure_dir(FileName),
   case file:open(FileName, [write, {delayed_write, 1024, 50}]) of
 		{ok, File} ->
-		  Header = flv:header(),
-    	file:write(File, Header),
-    	?MODULE:writer(#flv_file_writer{file = File});
+    	file:write(File, flv:header()),
+    	?MODULE:writer(#flv_file_writer{writer = fun(Data) ->
+    	  file:write(File, Data)
+    	end});
 		Error ->
 		  error_logger:error_msg("Failed to start recording stream to ~p because of ~p", [FileName, Error]),
 			exit({flv_file_writer, Error})
   end.
 	
-writer(#flv_file_writer{file = File, base_dts = BaseDTS} = Writer) ->
+writer(#flv_file_writer{base_dts = BaseDTS, writer = Writer} = FlvWriter) ->
   receive
     #video_frame{dts = DTS} = Frame ->
       {DTS1, BaseDTS1} = case BaseDTS of
         undefined -> {0, DTS};
         _ -> {DTS - BaseDTS, BaseDTS}
-      end,    
-    	file:write(File, flv_video_frame:to_tag(Frame#video_frame{dts = DTS1})),
-    	?MODULE:writer(Writer#flv_file_writer{base_dts = BaseDTS1});
+      end,
+    	Writer(flv_video_frame:to_tag(Frame#video_frame{dts = DTS1})),
+    	?MODULE:writer(FlvWriter#flv_file_writer{base_dts = BaseDTS1});
     Else ->
-      ?D({"flv_writer", Else})
+      ?D({"flv_writer", Else}),
+    	?MODULE:writer(FlvWriter)
   end.
   
