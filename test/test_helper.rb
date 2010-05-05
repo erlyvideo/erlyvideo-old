b2dfb9da26bc9f39a7f634ca11e092761231c471
Max Lapshin <max@maxidoors.ru>
2010-05-06 00:56:58 +0400

    iPhone segments tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,12 +8,21 @@ class Test::Unit::TestCase
   end
   
   def media_duration(url, options = nil)
-    lines = `ffmpeg #{options} -i #{url} 2>&1`
+    lines = `ffmpeg -timelimit 8 #{options} -i #{url} 2>&1`
     md = /Duration: ([^\ ,]+),/.match(lines)
     if md.captures.first =~ /(\d+):(\d+):([\d\.]+)/
       $1.to_i*3600 + $2.to_i*60 + $3.to_f
     else
       md.captures.first
+    end
+  end
+
+  def media_start(url, options = nil)
+    lines = `ffmpeg -timelimit 8 #{options} -i #{url} 2>&1`
+    if lines =~ /start: ([\d\.]+)/
+      $1.to_f
+    else
+      nil
     end
   end
 end
---
+++ test/unit/iphone_segment_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class IphoneSegmentTest < Test::Unit::TestCase
+  def setup
+    restart_erlyvideo
+  end
+  
+  def test_segment
+    start = media_start("http://localhost:8082/iphone/segments/video.mp4/1.ts")
+    assert start.is_a?(Numeric), "Start should be number: #{start.inspect}"
+    assert start > 9, "Start should be above 9 seconds: #{start}"
+    assert start < 12, "Start should be below 12 seconds: #{start}"
+
+    duration = media_duration("http://localhost:8082/iphone/segments/video.mp4/1.ts")
+    assert duration.is_a?(Numeric), "Duration should be number: #{duration.inspect}"
+    assert duration > 9, "Duration should be positive: #{duration}"
+    assert duration > 15, "Duration should be positive: #{duration}"
+  end
+end
