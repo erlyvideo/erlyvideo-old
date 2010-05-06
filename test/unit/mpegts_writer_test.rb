78baf4802c6fe88646b3eb3458c9cbc0b86abab8
Max Lapshin <max@maxidoors.ru>
2010-05-06 11:02:32 +0400

    Added  failing test for video.ts

--- test/unit/mpegts_writer_test.rb
+++ test/unit/mpegts_writer_test.rb
@@ -10,4 +10,13 @@ class MpegtsWriterTest < Test::Unit::TestCase
     assert duration.is_a?(Numeric), "Duration should be number: #{duration.inspect}"
     assert(duration > 0, "Duration should be positive: #{duration.inspect}")
   end
+
+  def test_read_video_ts
+    # video.mp4 is repacked into MPEG-TS
+    # MPEG-TS demuxer reads it
+    # and packs back to MPEG-TS
+    duration = media_duration("http://localhost:8082/stream/video.ts", "-fs 100000")
+    assert duration.is_a?(Numeric), "Duration should be number: #{duration.inspect}"
+    assert(duration > 0, "Duration should be positive: #{duration.inspect}")
+  end
 end
--- test/unit/rtmp_file_test.rb
+++ test/unit/rtmp_file_test.rb
@@ -6,11 +6,20 @@ class RtmpFileTest < Test::Unit::TestCase
   end
   
   def teardown
-    File.unlink("/tmp/test.flv")
+    File.unlink("/tmp/test.flv") if File.exists?("/tmp/test.flv")
   end
   
   def test_read_file
     `rtmpdump -r rtmp://localhost/vod/mp4:video --stop 5 -o /tmp/test.flv 2>&1`
+    duration = media_duration("/tmp/test.flv")
+    assert duration.is_a?(Numeric), "Duration should be number: #{duration.inspect}"
+    assert duration > 4, "Duration should be positive: #{duration}"
+  end
+
+  def test_read_file
+    File.unlink("/tmp/test.flv") if File.exists?("/tmp/test.flv")
+    result = `rtmpdump -r rtmp://localhost/vod/video.ts --stop 5 -o /tmp/test.flv 2>&1`
+    assert(File.size("/tmp/test.flv") > 0, "Should download file: #{result}")
     duration = media_duration("/tmp/test.flv")
     assert duration.is_a?(Numeric), "Duration should be number: #{duration.inspect}"
     assert duration > 4, "Duration should be positive: #{duration}"
