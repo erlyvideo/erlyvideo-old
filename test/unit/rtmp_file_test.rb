baa5cbc8af3a61f6d735c700f4ea68ca8f7360b1
Max Lapshin <max@maxidoors.ru>
2010-05-06 16:14:21 +0400

    Changed test for rtmp

--- test/unit/rtmp_file_test.rb
+++ test/unit/rtmp_file_test.rb
@@ -6,7 +6,7 @@ class RtmpFileTest < Test::Unit::TestCase
   end
   
   def teardown
-    File.unlink("/tmp/test.flv") if File.exists?("/tmp/test.flv")
+    # File.unlink("/tmp/test.flv") if File.exists?("/tmp/test.flv")
   end
   
   def test_read_file
@@ -18,10 +18,17 @@ class RtmpFileTest < Test::Unit::TestCase
 
   def test_read_stream
     File.unlink("/tmp/test.flv") if File.exists?("/tmp/test.flv")
-    result = Timeout::timeout(7) {`rtmpdump -r rtmp://localhost/vod/video.ts --stop 5 -o /tmp/test.flv 2>&1`} rescue true
+    result = begin
+      Timeout::timeout(7) {`rtmpdump -r rtmp://localhost/vod/video.ts --stop 5 -o /tmp/test.flv 2>&1`} 
+    rescue Timeout::Error
+      :timeout
+    end
     assert(File.size("/tmp/test.flv") > 0, "Should download file: #{result}")
-    duration = media_duration("/tmp/test.flv")
+    flvtool = `flvtool2 -D /tmp/test.flv 2>&1`
+    assert(flvtool.split(/\n/).last =~ /timestamp (\d+),/, "FLVTool should dump file")
+    duration = $1.to_i / 1000
     assert duration.is_a?(Numeric), "Duration should be number: #{duration.inspect}"
     assert duration > 4, "Duration should be positive: #{duration}"
+    File.unlink("/tmp/test.flv")
   end
 end
