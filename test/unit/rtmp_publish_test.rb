3b1586d5300d4b97f90e0eaef11e443f55acb85c
Max Lapshin <max@maxidoors.ru>
2010-05-11 17:48:13 +0400

    rtmp_publish should check iphone segments also

--- test/unit/rtmp_publish_test.rb
+++ test/unit/rtmp_publish_test.rb
@@ -16,7 +16,6 @@ class RtmpFileTest < Test::Unit::TestCase
   end
   
   def test_published_stream_is_shifted
-    
     File.unlink("/tmp/test.flv") if File.exists?("/tmp/test.flv")
     result = begin
       Timeout::timeout(7) {`rtmpdump -r rtmp://localhost/live/livestream --stop 5 -o /tmp/test.flv 2>&1`} 
@@ -25,7 +24,7 @@ class RtmpFileTest < Test::Unit::TestCase
     end
     assert(File.size("/tmp/test.flv") > 0, "Should download file: #{result}")
     
-    duration = media_duration("/tmp/test.flv")
+    duration = flvtool2_duration("/tmp/test.flv")
     assert duration.is_a?(Numeric), "Duration should be number: #{duration.inspect}"
     assert duration > 4, "Duration should be positive: #{duration}"
     
@@ -34,5 +33,20 @@ class RtmpFileTest < Test::Unit::TestCase
     assert_equal 0, start, "Start of rtmp stream should be strictly zero: #{start}"
     
     File.unlink("/tmp/test.flv")
+  end
+  
+  def test_iphone_of_live_stream
+    result = Timeout::timeout(7) {`curl -s http://localhost:8082/iphone/playlists/livestream.m3u8`}
+    assert_equal <<-EOF, result
+#EXTM3U
+#EXT-X-MEDIA-SEQUENCE:0
+#EXT-X-TARGETDURATION:10
+#EXT-X-ALLOW-CACHE:YES
+#EXTINF:10,
+/iphone/segments/livestream/0.ts
+    EOF
+
+    result = Timeout::timeout(7) {`curl -s http://localhost:8082/iphone/segments/livestream/0.ts`}
+    assert result.size > 10000, "Should download large content: size is #{result.size}"
   end
 end
