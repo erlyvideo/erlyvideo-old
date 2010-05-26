40978367e7b6aad7e0330faf9ba2d9f4dee355fc
Max Lapshin <max@maxidoors.ru>
2010-05-27 00:57:30 +0400

    Totally refactored video_frame structure

--- include/video_frame.hrl
+++ include/video_frame.hrl
@@ -1,15 +1,23 @@
+-type(frame_content() ::audio|video|metadata).
+-type(frame_flavor()  ::audio|video|metadata).
+-type(frame_video_codec() ::h264|sorensen|vp6|vp6f).
+-type(frame_audio_codec() ::aac|mp3|speex|nelly_moser|nelly_moser8).
+-type(frame_codec()   ::frame_video_codec()|frame_audio_codec()|atom()).
+
+-type(frame_sound_type() ::mono|stereo).
+-type(frame_sound_size() ::bit8|bit16).
+-type(frame_sound_rate() ::rate5|rate11|rate22|rate44).
+-type(frame_sound() ::{frame_sound_type(), frame_sound_size(), frame_sound_rate()}).
+
 -record(video_frame,{
-	type           = undefined,
-	dts            = undefined,
-	pts            = undefined,
-  decoder_config = false,
-	stream_id      = 0,
-	codec_id 	     = undefined,
-	frame_type     = undefined,
-	sound_type	   = undefined,
-	sound_size	   = undefined,
-	sound_rate	   = undefined,
-	body           = <<>>,
-	next_id        = undefined
-	}).
+	content        = undefined ::frame_content(),
+	dts            = undefined ::non_neg_number(),
+	pts            = undefined ::non_neg_number(),
+	stream_id      = 0         ::non_neg_integer(),
+	codec 	       = undefined ::frame_codec()|undefined,
+	flavor         = undefined ::frame_flavor(),
+	sound          = {undefined, undefined, undefined} ::frame_sound(),
+	body           = <<>>      ::binary(),
+	next_id        = undefined ::any()
+}).
 	
