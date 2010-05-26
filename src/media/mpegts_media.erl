81306297a55cf78e0cee9575b34c1681e38ed4bb
Max Lapshin <max@maxidoors.ru>
2010-05-26 21:50:57 +0400

    New mpegts_media

--- src/media/mpegts_media.erl
+++ src/media/mpegts_media.erl
@@ -32,6 +32,7 @@
 -author('Max Lapshin <max@maxidoors.ru>').
 -behaviour(ems_media).
 -include_lib("erlmedia/include/video_frame.hrl").
+-include("../../include/ems_media.hrl").
 
 
 -define(D(X), io:format("DEBUG ~p:~p ~p~n",[?MODULE, ?LINE, X])).
@@ -39,7 +40,7 @@
 -define(MAX_RESTART, 10).
 -define(TIMEOUT_RESTART, 1000).
 
--export([init/1, handle_frame/2, handle_control/2, handle_info/2]).
+-export([init/2, handle_frame/2, handle_control/2, handle_info/2]).
 
 -record(mpegts, {
   socket,
@@ -64,7 +65,7 @@
 %% @end
 %%----------------------------------------------------------------------
 
-init(Options) ->
+init(Media, Options) ->
   URL = proplists:get_value(url, Options),
   Type = proplists:get_value(type, Options),
   MakeRequest = case {Type, proplists:get_value(make_request, Options, true)} of
@@ -81,7 +82,8 @@ init(Options) ->
     _ -> ems_sup:start_mpegts_reader(self())
   end,
   ems_media:set_source(self(), Reader),
-  {ok, #mpegts{socket = Socket, demuxer = Reader, options = Options, url = URL, make_request = MakeRequest}}.
+  State = #mpegts{socket = Socket, demuxer = Reader, options = Options, url = URL, make_request = MakeRequest},
+  {ok, Media#ems_media{state = State}}.
 
 
   
@@ -106,7 +108,7 @@ handle_control({subscribe, _Client, _Options}, State) ->
   %% {ok, State} -> client is subscribed as active receiver
   %% {ok, State, tick} -> client requires ticker (file reader)
   %% {error, Reason} -> client receives {error, Reason}
-  {reply, ok, State};
+  {noreply, State};
 
 handle_control({source_lost, _Source}, State) ->
   %% Source lost returns:
@@ -118,17 +120,19 @@ handle_control({set_source, _Source}, State) ->
   %% Set source returns:
   %% {reply, Reply, State}
   %% {stop, Reason, State}
-  {reply, ok, State};
+  {noreply, State};
   
-handle_control({set_socket, Socket}, #mpegts{} = State) ->
+handle_control({set_socket, Socket}, #ems_media{state = State} = Media) ->
+  #mpegts{} = State,
   inet:setopts(Socket, [{active, once}]),
-  {reply, ok, State#mpegts{socket = Socket}};
+  State1 = State#mpegts{socket = Socket},
+  {noreply, Media#ems_media{state = State1}};
 
 handle_control(timeout, State) ->
   {stop, normal, State};
 
 handle_control(_Control, State) ->
-  {reply, ok, State}.
+  {noreply, State}.
 
 %%----------------------------------------------------------------------
 %% @spec (Frame::video_frame(), State) -> {reply, Frame, State} |
@@ -149,9 +153,9 @@ handle_frame(Frame, State) ->
 %% @doc Called by ems_media to parse incoming message.
 %% @end
 %%----------------------------------------------------------------------
-handle_info({http, Socket, {http_response, _Version, 200, _Reply}}, State) ->
+handle_info({http, Socket, {http_response, _Version, 200, _Reply}}, #ems_media{state = State} = Media) ->
   inet:setopts(Socket, [{active, once}]),
-  {noreply, State#mpegts{restart_count = undefined}};
+  {noreply, Media#ems_media{state = State#mpegts{restart_count = undefined}}};
 
 handle_info({http, Socket, {http_header, _, _Header, _, _Value}}, State) ->
   inet:setopts(Socket, [{active, once}]),
@@ -167,22 +171,25 @@ handle_info({tcp, Socket, Bin}, #mpegts{demuxer = Reader} = State) when Reader =
   Reader ! {data, Bin},
   {noreply, State};
 
-handle_info({tcp_closed, Socket}, #mpegts{restart_count = undefined} = State) ->
-  handle_info({tcp_closed, Socket}, State#mpegts{restart_count = 0});
+handle_info({tcp_closed, Socket}, #ems_media{state = #mpegts{restart_count = undefined}} = Media) ->
+  State = Media#ems_media.state,
+  State1 = State#mpegts{restart_count = 0},
+  handle_info({tcp_closed, Socket}, Media#ems_media{state = State1});
 
-handle_info({tcp_closed, _Socket}, #mpegts{url = URL, restart_count = Count, make_request = MakeRequest} = State) ->
+handle_info({tcp_closed, _Socket}, #ems_media{state = State} = Media) ->
+  #mpegts{url = URL, restart_count = Count, make_request = MakeRequest} = State,
   if
     Count > ?MAX_RESTART ->
-      {stop, normal, State};
+      {stop, normal, Media};
     MakeRequest == false ->
-      {noreply, State#mpegts{socket = undefined, restart_count = Count + 1}};
+      {noreply, Media#ems_media{state = State#mpegts{socket = undefined, restart_count = Count + 1}}};
     true ->  
       % FIXME
       % ems_event:stream_source_lost(Media#media_info.host, Media#media_info.name, self()),
       ?D({"Disconnected MPEG-TS/Shoutcast socket in mode", Count}),
       timer:sleep(100),
       Socket = connect_http(URL),
-      {noreply, State#mpegts{socket = Socket, restart_count = Count + 1}}
+      {noreply, Media#ems_media{state = State#mpegts{socket = Socket, restart_count = Count + 1}}}
   end;
 
 handle_info(Msg, State) ->
