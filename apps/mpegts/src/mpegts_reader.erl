8280652269f20851461ff16113cda9651ef3fbed
Max Lapshin <max@maxidoors.ru>
2011-07-02 09:39:07 +0400

    slight refactoring of mpegts_reader.hrl

--- apps/mpegts/src/mpegts_reader.erl
+++ apps/mpegts/src/mpegts_reader.erl
@@ -28,9 +28,45 @@
 -include("log.hrl").
 -include("../include/mpegts.hrl").
 -include("../include/mpegts_psi.hrl").
--include("mpegts_reader.hrl").
+% -include("mpegts_reader.hrl").
 
 -include_lib("erlmedia/include/video_frame.hrl").
+
+-define(MAX_PAYLOAD, 16#100000000).
+
+-record(decoder, {
+  buffer = <<>>,
+  pids = [],
+  consumer,
+  pmt_pid,
+  socket,
+  options,
+  byte_counter = 0,
+  program_info = [],
+  sdt,
+  current_time
+}).
+
+
+-record(stream, {
+  pid,
+  program_num,
+  demuxer,
+  handler,
+  codec,
+  ts_buffer = undefined,
+  es_buffer = <<>>,
+  counter = 0,
+  payload_size = ?MAX_PAYLOAD,
+  pcr,
+  start_dts,
+  dts,
+  pts,
+  video_config = undefined,
+  send_audio_config = false,
+  sample_rate,
+  h264
+}).
 
 
 -export([benchmark/0]).
@@ -281,7 +317,7 @@ ts_payload(<<_TEI:1, _Start:1, _Priority:1, _Pid:13, _Scrambling:2, 1:1, 1:1, _C
 
 ts_payload(<<_TEI:1, _Start:1, _Priority:1, _Pid:13, _Scrambling:2, 
               _Adaptation:1, 0:1, _Counter:4, _Payload/binary>>)  ->
-  ?D({"Empty payload on pid", _Pid}),
+  % ?D({"Empty payload on pid", _Pid}),
   <<>>.
 
 
