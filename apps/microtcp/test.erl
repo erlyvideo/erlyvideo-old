17790685a0fa5cbaabe1c30923b41458c175c492
Max Lapshin <max@maxidoors.ru>
2011-08-12 11:44:20 +0400

    microtcp small fixes

--- apps/microtcp/test.erl
+++ apps/microtcp/test.erl
@@ -2,9 +2,29 @@
 %%! -pa ebin
 
 main([]) ->
+  Listener = spawn(fun() ->
+    listen(9000)
+  end),
+  erlang:monitor(process, Listener),
+  
+  Client = spawn(fun() ->
+    connect(9000)
+  end),
+  erlang:monitor(process, Client),
+  
+  receive
+    {'DOWN', _, process, Client, Reason1} -> io:format("client died ~p~n", [Reason1])
+  end,
+  receive
+    {'DOWN', _, process, Listener, Reason2} -> io:format("listener died ~p~n", [Reason2])
+  end,
+  ok.
+
+
+listen(Port) ->
   Opts1 = [binary, {packet, raw}, {reuseaddr, true}, 
-          {keepalive, true}, {backlog, 30}, {active, false}],
-  {ok, Listen} = microtcp:listen(9000, Opts1),
+          {keepalive, true}, {backlog, 30}, {active, once}],
+  {ok, Listen} = microtcp:listen(Port, Opts1),
   io:format("Open port: ~p~n", [Listen]),
   receive
     {tcp_connection, Listen, Socket} ->
@@ -21,20 +41,29 @@ main([]) ->
   end,
   ok.
 
+-define(SIZE, 100000).
 
+connect(Port) ->
+  {ok, Sock} = gen_tcp:connect("localhost", Port, [binary]),
+  gen_tcp:recv(Sock, 0),
+  gen_tcp:recv(Sock, 0),
+  gen_tcp:recv(Sock, 0),
+  gen_tcp:close().
 
 client_launch() ->
-  Bin = crypto:rand_bytes(1000000),
+  Bin = crypto:rand_bytes(?SIZE),
   receive
     {socket, Socket} -> client_loop(Socket, Bin)
   end.
 
 client_loop(Socket, Bin) ->
-  microtcp:send(Socket, Bin),
+  microtcp:send(Socket, <<(size(Bin)):32, Bin/binary>>),
   receive
     {tcp_closed, Socket} -> ok;
-    Else -> io:format("Msg: ~p~n", [Else]),
-    client_loop(Socket, Bin)
+    Else -> 
+      io:format("Msg: ~p~n", [Else]),
+      inet:setopts(Socket, [{active,once}]),
+      client_loop(Socket, Bin)
   after
     40 -> client_loop(Socket, Bin)
   end.
