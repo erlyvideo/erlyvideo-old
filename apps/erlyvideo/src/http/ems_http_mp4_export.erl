8585619f6f98a90bded777fb46c12757df5d1297
Max Lapshin <max@maxidoors.ru>
2011-09-27 16:13:55 +0400

    should return 500 on mp4 export

--- apps/erlyvideo/src/http/ems_http_mp4_export.erl
+++ apps/erlyvideo/src/http/ems_http_mp4_export.erl
@@ -29,7 +29,6 @@
 http(Host, 'GET', ["mp4" | PathSpec], Req) ->
   Path = ems:pathjoin(PathSpec),
   
-  Req:stream(head, [{"Content-Type", "video/mp4"}, {"Connection", "close"}, {"Content-Disposition", "attachment; filename="++Path++".mp4"}]),
   {ok, Pid} = media_provider:open(Host, Path),
   Query = Req:parse_qs(),
   Options = case proplists:get_value("start", Query) of
@@ -39,6 +38,7 @@ http(Host, 'GET', ["mp4" | PathSpec], Req) ->
     undefined -> [];
     Duration -> [{duration, list_to_integer(Duration)*1000}]
   end,
+  Req:stream(head, [{"Content-Type", "video/mp4"}, {"Connection", "close"}, {"Content-Disposition", "attachment; filename="++Path++".mp4"}]),
   mp4_writer:dump_media(Pid, [{writer, fun(_Offset, Bin) ->
     Req:stream(Bin)
   end}|Options]),
