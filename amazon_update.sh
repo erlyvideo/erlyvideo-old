eaf4a6474307a50531619d1dc907087856b8f670
Ilya Shcherbak <ilya@erlyvideo.org>
2011-10-24 14:35:45 +0700

    execute patch

--- amazon_update.sh
+++ amazon_update.sh
@@ -3,8 +3,9 @@
 Host=$(./contrib/amazon_api get_host $1 $2 $3)
 echo $Host
 scp pk-*.pem root@$Host:/root/ec2
+scp curl_patch root@$Host:/root
 scp cert-*.pem root@$Host:/root/ec2
 scp ec2.pem root@$Host:/root/ec2/etc/ec2/amitools/cert-ec2.pem
 scp erlyvideo.tar.bz2 root@$Host:/root
-ssh  root@$Host '~/.bash_profile && ./script.sh'
+ssh  root@$Host '~/.bash_profile &&patch  ec2/lib/ec2/common/curl.rb < curl_patch && ./script.sh'
 ./contrib/amazon_api bterminate $1 $2 $3
