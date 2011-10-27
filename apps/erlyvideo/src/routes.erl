3451de8b4cf5af9ba0214485101172d2685b09a6
Ilya Shcherbak <ilya@erlyvideo.org>
2011-10-28 01:01:45 +0700

    Rewrite module

--- apps/erlyvideo/src/routes.erl
+++ apps/erlyvideo/src/routes.erl
@@ -7,34 +7,65 @@
 %% API
 -export([handler/1,handle_control/2]).
 
+-define(FILE_NAME,"routes.conf").
+
 -record(routes, {
 	  method,
 	  controller,
-	  handlers=[],
+	  routes=[],
 	  options=[]
 }).
 
 init([]) ->
   Handlers = 
-    [
-     "hds_handler",
-     "flv_handler",
-     "adobe_handler"
-    ],
-  {ok, #routes{handlers=Handlers}}.
+    case catch(ets:lookup(route_table,route)) of
+      {'EXIT',_Reason} ->
+	get_routes();
+      []->
+	get_routes();	
+      PList ->
+	io:format("~p~n",["Load from ETS table route_table.."]),
+	proplists:get_value(route,PList)
+    end,
+  {ok, #routes{routes=Handlers}}.
+
+get_routes()->
+  {ok,RList} = case file:read_file(?FILE_NAME) of
+    {ok,Data}->
+      {ok,erlang:binary_to_list(Data)};
+    _ -> errror
+  end,
+  TList=re:replace(RList,"(s\\\n)","",[global,{return,list}]),
+  {ok,Tokens,_}=erl_scan:string(TList),
+  {ok,List} = erl_parse:parse_term(Tokens),
+  Table=ets:new(route_table,[public,named_table]),
+  Handlers = 
+  [begin
+     NewURL = convert_to_pattern(URL,ExList),
+     {NewURL,Controller,Method} 
+   end||[{URL,Controller,Method,ExList}]<-List],
+  ets:insert(Table,{route,Handlers}),Handlers.
+
+convert_to_pattern(URL,ExList)->
+  Pattern1=exlist(URL,ExList),
+  Pattern2=case re:replace(Pattern1,"(:[a-zA-Z]+)","(.*)",[global,{return,list}]) of
+    Value2 when is_list(Value2) ->
+      Value2;
+    nomatch ->
+      ""
+  end,
+  Pattern2.
+
+exlist(URL,[])->
+  URL;
+exlist(URL,[{Name,Pattern}|ExList])->
+  List=re:replace(URL,Name,Pattern,[{return,list}]),
+  exlist(List,ExList).
 
 handler(URL) ->
    {ok,State}=init([]),
 %% Parsing URL
-  {Controller,Method,Params} = parse(URL,State),
-%% Request specific parser for Controller:Method
-  #routes{method=NewMethod,controller=NewController,options = NewOptions} =   case ?MODULE:handle_control({Controller,Method},State#routes{options=Params,controller=Controller}) of
-    {noreply,State} ->
-      {State,Params};
-    {noreply,State1} ->
-      State1
-  end,
-  {NewController,NewMethod,NewOptions}.
+   parse(URL,State).
 
 %%--------------------------------------------------------------------
 %% Internal handler 
@@ -64,13 +95,31 @@ handle_control(_Handler,State) ->
 %%% Internal functions
 %%%===================================================================
 
-parse(URL,_State) ->
-  ParamList=case re:run(URL,"http://([a-zA-Z0-9.]+):*([0-9]*)/*([-_a-zA-Z0-9.=/]*)",[{capture,all_but_first,binary}]) of
+parse(URL,#routes{routes=Routes}) ->
+  ParamList=case re:run(URL,"http://([a-zA-Z0-9.]+):*([0-9]*)/*([-_a-zA-Z0-9.=/]*)",[{capture,all_but_first,list}]) of
     {match,[_Host,_Port,RawParams]} ->
       RawParams;
      nomatch ->
       []
   end,
-  [Controller|Tail] = re:split(ParamList,"/|%3A|&"),
-  {Params,[Method]} = lists:split(length(Tail)-1,Tail),
-  {erlang:binary_to_list(Controller),erlang:binary_to_list(Method),[binary_to_list(Element)||Element <- Params]}.
+  Route = 
+    fun(List)->
+	Fun =
+	  fun
+	    (_F,[])->false;
+	    (F,[{X,Controller,Method}|Tail])->
+	      case re:run(ParamList,X,[{capture,all_but_first,list}]) of
+		{match,P}->
+		  {Controller,Method,P};
+		_->
+		  F(F,Tail)
+	      end
+	  end,
+	Fun(Fun,List)
+    end,
+  case Route(Routes) of
+    false ->
+      [];
+    VParams ->
+      VParams
+  end.
