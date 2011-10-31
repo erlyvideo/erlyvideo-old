483b2ecf06882ab1cc79284adf4e89563d0770a5
Ilya Shcherbak <ilya@erlyvideo.org>
2011-10-31 11:59:17 +0600

    Rewrite tests

--- apps/erlyvideo/src/routes.erl
+++ apps/erlyvideo/src/routes.erl
@@ -75,7 +75,7 @@ compose(PList,VList) ->
   compose(PList,VList,[]).
 
 compose([],[],Acc) ->
-  Acc;
+  lists:reverse(Acc);
 compose([Param|PTail],[[Var]|VTail],Acc) ->
   compose(PTail,VTail,[{Var,Param}|Acc]).
 
@@ -92,26 +92,25 @@ get_file_config()->
 
 get_routes(List)->
   catch(ets:new(route_table,[public,named_table])),
-
- Handlers = 
-  [begin
-     {URL,Controller,Method,ExList} = 
-       case Element of
-	 [{RURL,RController,RMethod,RExList}] ->
-	   {RURL,RController,RMethod,RExList};
-	 [{RURL,RController,RMethod}] ->
-	   {RURL,RController,RMethod,[]}
-       end,
-     VarList = 
-       case re:run(URL,"\\(*:([-_a-zA-Z0-9]+)\\)*",[{capture,all_but_first,list},global]) of
-	 {match,Vars} ->
-	   Vars;
-	 _ ->
-	   []
-       end, 
-     NewURL = convert_to_pattern(URL,ExList),
-     {NewURL,Controller,Method,VarList} 
-   end||Element <-List],
+  Handlers = 
+    [begin
+       {URL,Controller,Method,ExList} = 
+	 case Element of
+	   [{RURL,RController,RMethod,RExList}] ->
+	     {RURL,RController,RMethod,RExList};
+	   [{RURL,RController,RMethod}] ->
+	     {RURL,RController,RMethod,[]}
+	 end,
+       VarList = 
+	 case re:run(URL,"\\(*:([-_a-zA-Z0-9]+)\\)*",[{capture,all_but_first,list},global]) of
+	   {match,Vars} ->
+	     Vars;
+	   _ ->
+	     []
+	 end, 
+       NewURL = convert_to_pattern(URL,ExList),
+       {NewURL,Controller,Method,VarList} 
+     end||Element <-List],
 
   ets:insert(route_table,{route,Handlers}),Handlers.
 
@@ -139,12 +138,13 @@ exlist(URL,[{Name,Pattern}|ExList]) ->
 -include_lib("eunit/include/eunit.hrl").
 
 make_routes_test () ->
-  ?assertEqual([{"hds/(.*)/(.*)/Seg(.*)-frag(.*)",hds_handler,manifest}],get_routes([[{"hds/:video/:high/Seg(.*)-frag(.*)",hds_handler,manifest,[{":video","(.*)"}]}]])),
-  ?assertEqual([{"hds/(.*)/(.*)/Seg(.*)-frag(.*)",hds_handler,manifest}],get_routes([[{"hds/:video/:high/Seg(.*)-frag(.*)",hds_handler,manifest}]])).
+  ?assertEqual([{"hds/(.*)/(.*)/Seg(.*)-frag(.*)",hds_handler,manifest,[["video"],["high"],["segment"],["fragment"]]}],get_routes([[{"hds/:video/:high/Seg(:segment)-frag(:fragment)",hds_handler,manifest,[{":video","(.*)"}]}]])),
+    ?assertEqual([{"hds/(.*)/(.*)/Seg(.*)-frag(.*)",hds_handler,manifest,[["video"],["high"],["segment"],["fragment"]]}],get_routes([[{"hds/:video/:high/Seg(:segment)-frag(:fragment)",hds_handler,manifest}]])).
+
 
 complex_request_test () ->
-  ?assertEqual({hds_handler,manifest,["video.mp4","high","0","0"]},parse("http://my_host/hds/video.mp4/high/Seg0-frag0",#routes{routes=get_routes([[{"hds/:video/:high/Seg(.*)-frag(.*)",hds_handler,manifest}]])})).
+  ?assertEqual({hds_handler,manifest,[{"video","video.mp4"},{"quality","high"},{"segment","0"},{"fragment","1"}]},parse("http://my_host/hds/video.mp4/high/Seg0-frag1",#routes{routes=get_routes([[{"hds/:video/:quality/Seg(:segment)-frag(:fragment)",hds_handler,manifest}]])})).
 
 parse_request_test () ->
-  ?assertEqual({hds_handler,manifest,["video.mp4","high","0","0"]},parse("http://my_host/hds/video.mp4/high/Seg0-frag0",#routes{routes=[{"hds/(.*)/(.*)/Seg(.*)-frag(.*)",hds_handler,manifest}]})).
+  ?assertEqual({hds_handler,manifest,[{"video","video.mp4"},{"quality","high"},{"segment","0"},{"fragment","0"}]},parse("http://my_host/hds/video.mp4/high/Seg0-frag0",#routes{routes=[{"hds/(.*)/(.*)/Seg(.*)-frag(.*)",hds_handler,manifest,[["video"],["quality"],["segment"],["fragment"]]}]})).
 
