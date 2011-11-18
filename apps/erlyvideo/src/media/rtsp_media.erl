45a776808d40b25d810307a2217d05da0d4c7086
Ilya Shcherbak <ilya@erlyvideo.org>
2011-11-18 14:05:52 +0600

    move code from rtsp_socket

--- apps/erlyvideo/src/media/rtsp_media.erl
+++ apps/erlyvideo/src/media/rtsp_media.erl
@@ -25,6 +25,8 @@
 -behaviour(ems_media).
 -include("ems_media.hrl").
 -include("../log.hrl").
+-include_lib("erlmedia/include/video_frame.hrl").
+-include_lib("erlmedia/include/media_info.hrl").
 
 -export([init/2, handle_frame/2, handle_control/2, handle_info/2]).
 
@@ -93,10 +95,12 @@ handle_control(timeout, #ems_media{source = Reader} = Media) ->
   {noreply, Media};
 
 handle_control({make_request, URL}, #ems_media{state = #rtsp{timeout = Timeout}, options = Options}) ->
-  rtsp_socket:read(URL, [{consumer, self()},{timeout,Timeout},
+  Tracks = proplists:get_value(tracks,Options),
+  {ok,RTSP,#media_info{audio=A,video=V}=MediaInfo} = rtsp_socket:read(URL, [{consumer, self()},{timeout,Timeout},
                          {dump_traffic,proplists:get_value(dump_traffic,Options,true)},
                          {transport,proplists:get_value(transport,Options,tcp)},
-                         {tracks, proplists:get_value(tracks,Options)}]);
+                         {tracks, Tracks}]),
+  {ok, RTSP, MediaInfo#media_info{audio=rm_track_in_mediainfo(A,Tracks),video=rm_track_in_mediainfo(V,Tracks)}};
 
 handle_control(_Control, State) ->
   {noreply, State}.
@@ -127,10 +131,27 @@ handle_info(make_request, Media) ->
 handle_info(_Message, State) ->
   {noreply, State}.
 
+rm_track_in_mediainfo(Elements,[])->
+  Elements;
 
-
-
-
-
-
-
+rm_track_in_mediainfo(Elements,Tracks) ->
+  lists:foldl(
+    fun(Element,Acc) ->
+	NewElement=
+	  lists:foldl(
+	    fun(T,Acc1) -> 
+		CurTrack = "track"++erlang:integer_to_list(T),
+		case proplists:get_value(control,Element#stream_info.options) of
+		  CurTrack->
+		    [Element|Acc1];
+		  _->
+			Acc1
+		end
+	    end,[],Tracks),
+	case NewElement of
+	  [] ->
+	    Acc;
+	  [Else] ->
+	    [Else|Acc]
+	end
+    end,[],Elements).
